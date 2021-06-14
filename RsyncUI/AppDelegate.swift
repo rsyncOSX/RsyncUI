c88304b53ac28a2376258c85ed92bbbf9e2074fc
Thomas Evensen <thomeven@gmail.com>
2021-06-14 08:53:29 +0200

    Update AppDelegate.swift

--- RsyncUI/AppDelegate.swift
+++ RsyncUI/AppDelegate.swift
@@ -5,100 +5,8 @@
 //  Created by Thomas Evensen on 12/01/2021.
 //
 
-import Cocoa
 import SwiftUI
 import UserNotifications
-
-// @main
-class AppDelegate: NSObject, NSApplicationDelegate {
-    var window: NSWindow!
-
-    func applicationDidFinishLaunching(_: Notification) {
-        let center = UNUserNotificationCenter.current()
-        let options: UNAuthorizationOptions = [.alert, .badge, .sound]
-        center.requestAuthorization(options: options) { granted, _ in
-            if granted {
-                // application.registerForRemoteNotifications()
-            }
-        }
-        // Create base profile catalog
-        CatalogProfile().createrootprofilecatalog()
-        ReadUserConfigurationPLIST()
-        Running()
-        // Create the window and set the content view.
-        window = NSWindow(
-            contentRect: NSRect(x: 0, y: 0, width: 950, height: 600),
-            styleMask: [.titled, .closable, .miniaturizable, .resizable, .fullSizeContentView],
-            backing: .buffered, defer: false
-        )
-        window.center()
-        window.setFrameAutosaveName("Main Window")
-        window.contentView = NSHostingView(rootView:
-            RsyncUIView()
-                .environmentObject(GetRsyncversion())
-                .environmentObject(Profilenames())
-        )
-        window.makeKeyAndOrderFront(nil)
-        window.title = "RsyncUI"
-        window.isMovableByWindowBackground = true
-    }
-
-    func applicationShouldTerminateAfterLastWindowClosed(_: NSApplication) -> Bool {
-        return true
-    }
-
-    func applicationWillTerminate(_: Notification) {
-        // Insert code here to tear down your application
-    }
-}
-
-extension AppDelegate {
-    @IBAction func AboutPanel(_ sender: Any?) {
-        let content = NSViewController()
-        content.title = NSLocalizedString("About", comment: "about")
-        let view = NSHostingView(rootView: AboutView())
-        view.frame.size = view.fittingSize
-        content.view = view
-        let panel = NSPanel(contentViewController: content)
-        panel.styleMask = [.closable, .titled]
-        panel.orderFront(sender)
-        panel.makeKey()
-    }
-
-    @IBAction func showPreferences(_ sender: Any?) {
-        let content = NSViewController()
-        content.title = NSLocalizedString("RsyncUI settings", comment: "settings")
-        let view = NSHostingView(rootView: Usersettings()
-            .environmentObject(SharedReference.shared.errorobject!)
-            .environmentObject(GetRsyncversion()))
-        view.frame.size = view.fittingSize
-        content.view = view
-        let panel = NSPanel(contentViewController: content)
-        panel.styleMask = [.closable, .titled, .resizable]
-        panel.orderFront(sender)
-        panel.makeKey()
-    }
-
-    @IBAction func logview(_ sender: Any?) {
-        let content = NSViewController()
-        content.title = NSLocalizedString("RsyncUI logfile", comment: "settings")
-        let view = NSHostingView(rootView: LogfileView())
-        view.frame.size = view.fittingSize
-        content.view = view
-        let panel = NSPanel(contentViewController: content)
-        panel.styleMask = [.closable, .titled, .resizable]
-        panel.orderFront(sender)
-        panel.makeKey()
-    }
-
-    @IBAction func schedules(_: Any?) {
-        let running = Running()
-        guard running.informifisrsyncshedulerunning() == false else { return }
-        NSWorkspace.shared.open(URL(fileURLWithPath: (SharedReference.shared.pathrsyncschedule ?? "/Applications/")
-                + SharedReference.shared.namersyncschedule))
-        NSApp.terminate(self)
-    }
-}
 
 @main
 struct RsyncUIApp: App {
@@ -229,3 +137,53 @@ struct ContentView: View {
         })
     }
 }
+
+/*
+ extension AppDelegate {
+     @IBAction func AboutPanel(_ sender: Any?) {
+         let content = NSViewController()
+         content.title = NSLocalizedString("About", comment: "about")
+         let view = NSHostingView(rootView: AboutView())
+         view.frame.size = view.fittingSize
+         content.view = view
+         let panel = NSPanel(contentViewController: content)
+         panel.styleMask = [.closable, .titled]
+         panel.orderFront(sender)
+         panel.makeKey()
+     }
+
+     @IBAction func showPreferences(_ sender: Any?) {
+         let content = NSViewController()
+         content.title = NSLocalizedString("RsyncUI settings", comment: "settings")
+         let view = NSHostingView(rootView: Usersettings()
+             .environmentObject(SharedReference.shared.errorobject!)
+             .environmentObject(GetRsyncversion()))
+         view.frame.size = view.fittingSize
+         content.view = view
+         let panel = NSPanel(contentViewController: content)
+         panel.styleMask = [.closable, .titled, .resizable]
+         panel.orderFront(sender)
+         panel.makeKey()
+     }
+
+     @IBAction func logview(_ sender: Any?) {
+         let content = NSViewController()
+         content.title = NSLocalizedString("RsyncUI logfile", comment: "settings")
+         let view = NSHostingView(rootView: LogfileView())
+         view.frame.size = view.fittingSize
+         content.view = view
+         let panel = NSPanel(contentViewController: content)
+         panel.styleMask = [.closable, .titled, .resizable]
+         panel.orderFront(sender)
+         panel.makeKey()
+     }
+
+     @IBAction func schedules(_: Any?) {
+         let running = Running()
+         guard running.informifisrsyncshedulerunning() == false else { return }
+         NSWorkspace.shared.open(URL(fileURLWithPath: (SharedReference.shared.pathrsyncschedule ?? "/Applications/")
+                 + SharedReference.shared.namersyncschedule))
+         NSApp.terminate(self)
+     }
+ }
+ */
