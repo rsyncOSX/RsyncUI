f8868bae3c03017aece7aa5da4903a050cc2dac4
rsyncOSX <thomeven@gmail.com>
2021-06-14 06:41:11 +0200

    Update AddConfigurationView.swift

--- RsyncUI/Views/Add/AddConfigurationView.swift
+++ RsyncUI/Views/Add/AddConfigurationView.swift
@@ -297,7 +297,8 @@ struct AddConfigurationView: View {
     }
 
     var cannotdeletedefaultprofile: some View {
-        AlertToast(type: .error(Color.red), title: Optional(NSLocalizedString("Cannot delete default profile", comment: "settings")), subTitle: Optional(""))
+        AlertToast(type: .error(Color.red),
+                   title: Optional(NSLocalizedString("Cannot delete default profile", comment: "settings")), subTitle: Optional(""))
     }
 
     var profile: String? {
