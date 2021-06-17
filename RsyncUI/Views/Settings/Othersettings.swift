0eb3f556810ddf487c569524de6c8af681c3ae9b
Thomas Evensen <thomeven@gmail.com>
2021-06-17 08:15:08 +0200

    update

--- RsyncUI/Views/Settings/Othersettings.swift
+++ RsyncUI/Views/Settings/Othersettings.swift
@@ -4,17 +4,10 @@
 //
 //  Created by Thomas Evensen on 03/03/2021.
 //
-
 import SwiftUI
 
 struct Othersettings: View {
     @StateObject var usersettings = ObserveablePath()
-    // Documents about convert
-    var infoaboutconvert: String = "https://rsyncui.netlify.app/post/plist/"
-    @State private var convertisready: Bool = false
-    @State private var jsonfileexists: Bool = false
-    @State private var convertisconfirmed: Bool = false
-    @State private var convertcompleted: Bool = false
     @State private var backup: Bool = false
 
     var body: some View {
@@ -44,18 +37,6 @@ struct Othersettings: View {
                     Spacer()
                 }
 
-                if convertcompleted == true {
-                    AlertToast(type: .complete(Color.green),
-                               title: Optional(NSLocalizedString("Completed",
-                                                                 comment: "settings")), subTitle: Optional(""))
-                        .onAppear(perform: {
-                            // Show updated for 1 second
-                            DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
-                                convertcompleted = false
-                            }
-                        })
-                }
-
                 if backup == true {
                     AlertToast(type: .complete(Color.green),
                                title: Optional(NSLocalizedString("Saved",
@@ -68,41 +49,17 @@ struct Othersettings: View {
                         })
                 }
             }
-
-            if convertisready {
-                HStack {
-                    Spacer()
-
-                    prepareconvertplist
-
-                    Spacer()
-                }
-
-                HStack {
-                    Spacer()
-
-                    if jsonfileexists { alertjsonfileexists }
-
-                    Spacer()
-                }
-            }
-
             // Save button right down corner
             Spacer()
 
             HStack {
                 Spacer()
 
-                convertbutton
-
                 usersetting
             }
         }
         .lineSpacing(2)
         .padding()
-        .onAppear(perform: {
-            convertisready = false
-        })
     }
 
     // Save usersetting is changed
@@ -121,11 +78,6 @@ struct Othersettings: View {
             }
         }
         .disabled(!usersettings.isDirty)
-    }
-
-    var convertbutton: some View {
-        Button(NSLocalizedString("PLIST", comment: "Othersettings")) { verifyconvert() }
-            .buttonStyle(PrimaryButtonStyle())
     }
 
     // Environment
@@ -185,30 +137,6 @@ struct Othersettings: View {
                 }
             })
     }
-
-    var prepareconvertplist: some View {
-        HStack {
-            Button(NSLocalizedString("Info about convert", comment: "Othersettings")) { openinfo() }
-                .buttonStyle(PrimaryButtonStyle())
-
-            ToggleView(NSLocalizedString("Confirm convert", comment: "Othersettings"), $convertisconfirmed)
-
-            if convertisconfirmed {
-                VStack {
-                    // Backup configuration files
-                    Button(NSLocalizedString("Backup", comment: "usersetting")) { backupuserconfigs() }
-                        .buttonStyle(PrimaryButtonStyle())
-
-                    Button(NSLocalizedString("Convert", comment: "Othersettings")) { convert() }
-                        .buttonStyle(PrimaryButtonStyle())
-                }
-            }
-        }
-    }
-
-    var alertjsonfileexists: some View {
-        AlertToast(type: .error(Color.red), title: Optional("JSON file exists"), subTitle: Optional(""))
-    }
 }
 
 extension Othersettings {
@@ -216,40 +144,6 @@ extension Othersettings {
         usersettings.isDirty = false
         usersettings.inputchangedbyuser = false
         _ = WriteUserConfigurationPLIST()
-    }
-
-    /*
-     TODO: must fix
-     */
-    func verifyconvert() {
-        /*
-         let configs = ReadConfigurationsPLIST(rsyncUIData.profile)
-         if configs.thereisplistdata == true {
-             convertisready = true
-         }
-         if configs.jsonfileexist == true {
-             jsonfileexists = true
-         }
-         */
-    }
-
-    func convert() {
-        /*
-         let configs = ReadConfigurationsPLIST(rsyncUIData.profile)
-         let schedules = ReadSchedulesPLIST(rsyncUIData.profile)
-         if convertisconfirmed {
-             configs.writedatatojson()
-             schedules.writedatatojson()
-         }
-         convertisready = false
-         jsonfileexists = false
-         convertisconfirmed = false
-         convertcompleted = true
-         */
-    }
-
-    func openinfo() {
-        NSWorkspace.shared.open(URL(string: infoaboutconvert)!)
     }
 
     func backupuserconfigs() {
