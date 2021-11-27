718a908f2a73e7f81039afd9e4e20becf8edd3fc
Thomas Evensen <22427340+rsyncOSX@users.noreply.github.com>
2021-11-27 12:07:54 +0100

    update

--- RsyncUI/Views/Multipletasks/MultipletasksView.swift
+++ RsyncUI/Views/Multipletasks/MultipletasksView.swift
@@ -10,6 +10,7 @@ import SwiftUI
 
 struct MultipletasksView: View {
     @EnvironmentObject var rsyncUIdata: RsyncUIconfigurations
+    @EnvironmentObject var outputfromrsync: OutputFromRsync
     // The object holds the progressdata for the current estimated task
     // which is executed. Data for progressview.
     @EnvironmentObject var executedetails: InprogressCountExecuteOneTaskDetails
@@ -36,6 +37,22 @@ struct MultipletasksView: View {
     @State private var focusstarttestfortcpconnections: Bool = false
     @State private var searchText: String = ""
 
+    // Singletasks
+    // Execute estimate and execution
+    @StateObject private var singletaskstate = SingleTaskState()
+    // Execute singletask, no estimation
+    @StateObject private var singletasknowstate = SingletaskNowState()
+    // Output from tasks are summarized and reported to the inprogresscountrsyncoutput object
+    // Also progress about synchronizing, if estimated first
+    @StateObject private var inprogresscountrsyncoutput = InprogressCountRsyncOutput()
+    // Must be a @State because it is changed
+    @State private var executesingletasks: ExecuteSingleTask?
+    @State private var executetasknow: ExecuteSingleTaskNow?
+    @State private var executestate: SingleTaskWork = .start
+    @State private var presentsheetview = false
+    // If shellout
+    @State private var shellout: Bool = false
+
     var body: some View {
         ZStack {
             ConfigurationsListSelectable(selectedconfig: $selectedconfig.onChange { reset() },
@@ -46,7 +63,6 @@ struct MultipletasksView: View {
             if focusstartestimation { labelshortcutestimation }
             if focusstartexecution { labelshortcutexecute }
             if focusstarttestfortcpconnections { notifyverifyTCPconnections }
-            // if estimationstate.estimationstate == .estimate { progressviewestimation }
         }
 
         HStack {
@@ -82,13 +98,6 @@ struct MultipletasksView: View {
         .focusedSceneValue(\.startestimation, $focusstartestimation)
         .focusedSceneValue(\.startexecution, $focusstartexecution)
         .focusedSceneValue(\.starttestfortcpconnections, $focusstarttestfortcpconnections)
-        /*
-         .onAppear(perform: {
-             if selectedprofile == nil {
-                 selectedprofile = SharedReference.shared.defaultprofile
-             }
-         })
-          */
     }
 
     var progressviewestimation: some View {
@@ -254,5 +263,119 @@ extension MultipletasksView {
             print(tcpconnections.indexBoolremoteserverOff ?? [])
         }
         focusstarttestfortcpconnections = false
+    }
+}
+
+extension MultipletasksView {
+    func completed() {
+        if let output = inprogresscountrsyncoutput.getoutput() {
+            outputfromrsync.setoutput(output: output)
+        }
+        reload = true
+        selecteduuids.removeAll()
+        singletaskstate.updatestate(state: .start)
+        singletasknowstate.updatestate(state: .start)
+        inprogresscountrsyncoutput.resetcounts()
+        executesingletasks = nil
+        executetasknow = nil
+        singletaskstate.estimateonly = false
+    }
+
+    func estimatesingletask() {
+        singletaskstate.estimateonly = true
+        setuuidforsingletask()
+        executeestimatedsingletask()
+    }
+
+    // Changed
+    func presentrsyncoutput() {
+        presentsheetview = true
+    }
+
+    func executeestimatedsingletask() {
+        switch singletaskstate.singletaskstate {
+        case .start:
+            executesingletasks = nil
+            executetasknow = nil
+            singletaskstate.updatestate(state: .estimate)
+            executesingletasks = ExecuteSingleTask(uuids: selecteduuids,
+                                                   profile: rsyncUIdata.configurationsfromstore?.profile,
+                                                   configurationsSwiftUI: rsyncUIdata.configurationsfromstore?.configurationData,
+                                                   singletaskstate: singletaskstate,
+                                                   updateinprogresscount: inprogresscountrsyncoutput)
+            executesingletasks?.estimate()
+        case .execute:
+            singletaskstate.updatestate(state: .estimated)
+            executesingletasks?.execute()
+        default:
+            return
+        }
+    }
+
+    // Changed
+    func abortsingletask() {
+        singletaskstate.updatestate(state: .start)
+        singletasknowstate.updatestate(state: .start)
+        executesingletasks?.abort()
+        executetasknow?.abort()
+        executesingletasks = nil
+        executetasknow = nil
+        inprogresscountrsyncoutput.resetcounts()
+        // kill any ongoing processes
+        _ = InterruptProcess()
+        reload = true
+    }
+
+    func resetexecutestate() {
+        singletaskstate.updatestate(state: .start)
+        singletasknowstate.updatestate(state: .start)
+        executesingletasks = nil
+        executetasknow = nil
+        inprogresscountrsyncoutput.resetcounts()
+        if let config = selectedconfig {
+            if (config.executepretask ?? -1) == 1 {
+                shellout = true
+            } else {
+                shellout = false
+            }
+        }
+        // kill any ongoing processes
+        _ = InterruptProcess()
+    }
+
+    func executesingletasknow() {
+        executesingletasks = nil
+        executetasknow = nil
+        setuuidforsingletask()
+        guard selecteduuids.count == 1 else { return }
+        singletasknowstate.updatestate(state: .execute)
+        if let config = selectedconfig {
+            if PreandPostTasks(config: config).executepretask || PreandPostTasks(config: config).executeposttask {
+                executetasknow =
+                    ExecuteSingleTaskNowShellout(uuids: selecteduuids,
+                                                 profile: rsyncUIdata.configurationsfromstore?.profile,
+                                                 configurationsSwiftUI: rsyncUIdata.configurationsfromstore?.configurationData,
+                                                 executetaskstate: singletasknowstate,
+                                                 updateinprogresscount: inprogresscountrsyncoutput)
+            } else {
+                executetasknow =
+                    ExecuteSingleTaskNow(uuids: selecteduuids,
+                                         profile: rsyncUIdata.configurationsfromstore?.profile,
+                                         configurationsSwiftUI: rsyncUIdata.configurationsfromstore?.configurationData,
+                                         executetaskstate: singletasknowstate,
+                                         updateinprogresscount: inprogresscountrsyncoutput)
+            }
+        }
+    }
+
+    func setuuidforsingletask() {
+        selecteduuids.removeAll()
+        if let sel = selectedconfig,
+           let index = rsyncUIdata.configurations?.firstIndex(of: sel)
+        {
+            if let id = rsyncUIdata.configurations?[index].id {
+                selecteduuids.insert(id)
+            }
+        }
     }
 }
