30d6e257095b017e311c999b80a04514573927f5
Thomas Evensen <22427340+rsyncOSX@users.noreply.github.com>
2022-02-13 09:29:58 +0100

    update

--- RsyncUI/Model/Storage/Basic/JSON/DecodeUserConfiguration.swift
+++ RsyncUI/Model/Storage/Basic/JSON/DecodeUserConfiguration.swift
@@ -9,18 +9,35 @@ import Foundation
 
 struct DecodeUserConfiguration: Codable {
     let rsyncversion3: Int?
+    // Detailed logging
+    let detailedlogging: Int?
+    // Logging to logfile
+    let minimumlogging: Int?
+    let fulllogging: Int?
+    let nologging: Int?
 
     enum CodingKeys: String, CodingKey {
         case rsyncversion3
+        case detailedlogging
+        case minimumlogging
+        case fulllogging
+        case nologging
     }
 
     init(from decoder: Decoder) throws {
         let values = try decoder.container(keyedBy: CodingKeys.self)
         rsyncversion3 = try values.decodeIfPresent(Int.self, forKey: .rsyncversion3)
+        detailedlogging = try values.decodeIfPresent(Int.self, forKey: .detailedlogging)
+        minimumlogging = try values.decodeIfPresent(Int.self, forKey: .minimumlogging)
+        fulllogging = try values.decodeIfPresent(Int.self, forKey: .fulllogging)
+        nologging = try values.decodeIfPresent(Int.self, forKey: .nologging)
     }
 
-    // This init is used in WriteScheduleJSON
-    init(_ data: UserConfiguration) {
-        rsyncversion3 = data.rsyncversion3
+    init(_ userconfiguration: UserConfiguration) {
+        rsyncversion3 = userconfiguration.rsyncversion3
+        detailedlogging = userconfiguration.detailedlogging
+        minimumlogging = userconfiguration.minimumlogging
+        fulllogging = userconfiguration.fulllogging
+        nologging = userconfiguration.nologging
     }
 }
--- RsyncUI/Model/Storage/Basic/UserConfiguration.swift
+++ RsyncUI/Model/Storage/Basic/UserConfiguration.swift
@@ -8,16 +8,24 @@
 import Foundation
 
 struct UserConfiguration: Codable {
-    var rsyncversion3: Int?
+    var rsyncversion3: Int = -1
+    // Detailed logging
+    var detailedlogging: Int = 1
+    // Logging to logfile
+    var minimumlogging: Int = -1
+    var fulllogging: Int = -1
+    var nologging: Int = 1
 
     // Used when reading JSON data from store
     // see in ReadScheduleJSON
     init(_ data: DecodeUserConfiguration) {
         rsyncversion3 = data.rsyncversion3 ?? -1
+        detailedlogging = data.detailedlogging ?? 1
+        minimumlogging = data.minimumlogging ?? -1
+        fulllogging = data.fulllogging ?? -1
+        nologging = data.nologging ?? 1
     }
 
-    // Create an empty record with no values
-    init() {
-        rsyncversion3 = -1
-    }
+    // Default values user configuration
+    init() {}
 }
