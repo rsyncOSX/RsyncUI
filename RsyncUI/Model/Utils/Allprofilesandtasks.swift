a2dc1f9d0788d8b6ee7085e22b2ab7e67bfc6177
Thomas Evensen <thomeven_github@icloud.com>
2023-03-21 11:27:47 +0100

    update

---
+++ RsyncUI/Model/Utils/Allprofilesandtasks.swift
@@ -0,0 +1,42 @@
+//
+//  Allprofilesandtasks.swift
+//  RsyncUI
+//
+//  Created by Thomas Evensen on 21/03/2023.
+//
+
+class Allprofilesandtasks {
+    // Configurations object
+    var alltasks: [Configuration]?
+    var allprofiles: [String]?
+    var alloffsiteservers: [String]?
+
+    private func getprofilenames() {
+        allprofiles = Catalogsandfiles(.configurations).getcatalogsasstringnames()
+    }
+
+    private func readalltasks() {
+        var configurations: [Configuration]?
+        for i in 0 ..< (allprofiles?.count ?? 0) {
+            let profilename = allprofiles?[i]
+            if alltasks == nil { alltasks = [] }
+            if profilename == "Default profile" {
+                configurations = ReadConfigurationJSON(nil).configurations
+            } else {
+                configurations = ReadConfigurationJSON(profilename).configurations
+            }
+            for j in 0 ..< (configurations?.count ?? 0) {
+                configurations?[j].profile = profilename
+                if let configuration = configurations?[j] {
+                    alltasks?.append(configuration)
+                }
+            }
+        }
+    }
+
+    init() {
+        getprofilenames()
+        alloffsiteservers = [String]()
+        readalltasks()
+    }
+}
