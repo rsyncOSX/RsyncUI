e675cd38110dec587eebb2bc69ca78f7d2c9e4c6
rsyncOSX <thomeven@gmail.com>
2023-05-18 23:09:04 +0200

    Update TableListofTasksProgress.swift

--- RsyncUI/Views/Configurations/TableListofTasksProgress.swift
+++ RsyncUI/Views/Configurations/TableListofTasksProgress.swift
@@ -123,3 +123,10 @@ struct TableListofTasksProgress: View {
         return false
     }
 }
+
+extension Sequence where Iterator.Element: Hashable {
+    func unique() -> [Iterator.Element] {
+        var seen: Set<Iterator.Element> = []
+        return filter { seen.insert($0).inserted }
+    }
+}
