5284a085a9f69164b0f6806ea2b2f016b8b3badc
rsyncOSX <thomeven@gmail.com>
2023-05-23 14:27:00 +0200

    Update ConfigurationsListSmall.swift

--- RsyncUI/Views/Configurations/ConfigurationsListSmall.swift
+++ RsyncUI/Views/Configurations/ConfigurationsListSmall.swift
@@ -12,9 +12,6 @@ struct ConfigurationsListSmall: View {
     @EnvironmentObject var rsyncUIdata: RsyncUIconfigurations
     @Binding var selectedconfig: Configuration?
     @Binding var reload: Bool
-    // Alert for delete
-    @State private var confirmationShown = false
-    @State private var selecteduuids = Set<UUID>()
 
     var body: some View {
         configlist
