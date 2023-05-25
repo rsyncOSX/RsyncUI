6582df717dbf3f664b776f8fcbd6e35750fe7cef
rsyncOSX <thomeven@gmail.com>
2023-05-25 08:31:02 +0200

    Update DetailsView.swift

--- RsyncUI/Views/Detailsview/DetailsView.swift
+++ RsyncUI/Views/Detailsview/DetailsView.swift
@@ -21,62 +21,124 @@ struct DetailsView: View {
         VStack {
             ZStack {
                 VStack {
-                    Table(estimateddataonetask.estimatedlistonetask) {
-                        TableColumn("Synchronize ID", value: \.backupID)
-                            .width(min: 100, max: 200)
-                        TableColumn("Task", value: \.task)
-                            .width(max: 80)
-                        TableColumn("Local catalog", value: \.localCatalog)
-                            .width(min: 80, max: 300)
-                        TableColumn("Remote catalog", value: \.offsiteCatalog)
-                            .width(min: 80, max: 300)
-                        TableColumn("Server", value: \.offsiteServer)
-                            .width(max: 70)
-                        TableColumn("User", value: \.offsiteUsername)
-                            .width(max: 50)
-                    }
-                    .frame(width: 650, height: 50, alignment: .center)
-                    .foregroundColor(.blue)
+                    if #available(macOS 13.0, *) {
+                        Form {
+                            if gettingremotedata == false {
+                                HStack {
+                                    VStack(alignment: .leading) {
+                                        LabeledContent("Synchronize ID: ") {
+                                            Text(estimateddataonetask.estimatedlistonetask[0].backupID)
+                                                .foregroundColor(.blue)
+                                        }
+                                        LabeledContent("Task: ") {
+                                            Text(estimateddataonetask.estimatedlistonetask[0].task)
+                                                .foregroundColor(.blue)
+                                        }
+                                        LabeledContent("Local catalog: ") {
+                                            Text(estimateddataonetask.estimatedlistonetask[0].localCatalog)
+                                                .foregroundColor(.blue)
+                                        }
+                                        LabeledContent("Remote catalog: ") {
+                                            Text(estimateddataonetask.estimatedlistonetask[0].offsiteCatalog)
+                                                .foregroundColor(.blue)
+                                        }
+                                        LabeledContent("Server: ") {
+                                            Text(estimateddataonetask.estimatedlistonetask[0].offsiteServer)
+                                                .foregroundColor(.blue)
+                                        }
+                                    }
 
-                    Table(estimateddataonetask.estimatedlistonetask) {
-                        TableColumn("New") { files in
-                            Text(files.newfiles)
-                                .frame(maxWidth: .infinity, alignment: .trailing)
+                                    VStack(alignment: .leading) {
+                                        LabeledContent("New: ") {
+                                            Text(estimateddataonetask.estimatedlistonetask[0].newfiles)
+                                                .foregroundColor(.blue)
+                                        }
+                                        LabeledContent("Delete: ") {
+                                            Text(estimateddataonetask.estimatedlistonetask[0].deletefiles)
+                                                .foregroundColor(.blue)
+                                        }
+                                        LabeledContent("Files: ") {
+                                            Text(estimateddataonetask.estimatedlistonetask[0].transferredNumber)
+                                                .foregroundColor(.blue)
+                                        }
+                                        LabeledContent("Bytes: ") {
+                                            Text(estimateddataonetask.estimatedlistonetask[0].transferredNumberSizebytes)
+                                                .foregroundColor(.blue)
+                                        }
+                                        LabeledContent("Tot num: ") {
+                                            Text(estimateddataonetask.estimatedlistonetask[0].totalNumber)
+                                                .foregroundColor(.blue)
+                                        }
+                                        LabeledContent("Tot bytes: ") {
+                                            Text(estimateddataonetask.estimatedlistonetask[0].totalNumberSizebytes)
+                                                .foregroundColor(.blue)
+                                        }
+                                        LabeledContent("Tot dir: ") {
+                                            Text(estimateddataonetask.estimatedlistonetask[0].totalDirs)
+                                                .foregroundColor(.blue)
+                                        }
+                                    }
+                                }
+                            }
                         }
-                        .width(max: 40)
-                        TableColumn("Delete") { files in
-                            Text(files.deletefiles)
-                                .frame(maxWidth: .infinity, alignment: .trailing)
+                    } else {
+                        Table(estimateddataonetask.estimatedlistonetask) {
+                            TableColumn("Synchronize ID", value: \.backupID)
+                                .width(min: 100, max: 200)
+                            TableColumn("Task", value: \.task)
+                                .width(max: 80)
+                            TableColumn("Local catalog", value: \.localCatalog)
+                                .width(min: 80, max: 300)
+                            TableColumn("Remote catalog", value: \.offsiteCatalog)
+                                .width(min: 80, max: 300)
+                            TableColumn("Server", value: \.offsiteServer)
+                                .width(max: 70)
+                            TableColumn("User", value: \.offsiteUsername)
+                                .width(max: 50)
                         }
-                        .width(max: 40)
-                        TableColumn("Files") { files in
-                            Text(files.transferredNumber)
-                                .frame(maxWidth: .infinity, alignment: .trailing)
+                        .frame(width: 650, height: 50, alignment: .center)
+                        .foregroundColor(.blue)
+
+                        Table(estimateddataonetask.estimatedlistonetask) {
+                            TableColumn("New") { files in
+                                Text(files.newfiles)
+                                    .frame(maxWidth: .infinity, alignment: .trailing)
+                            }
+                            .width(max: 40)
+                            TableColumn("Delete") { files in
+                                Text(files.deletefiles)
+                                    .frame(maxWidth: .infinity, alignment: .trailing)
+                            }
+                            .width(max: 40)
+                            TableColumn("Files") { files in
+                                Text(files.transferredNumber)
+                                    .frame(maxWidth: .infinity, alignment: .trailing)
+                            }
+                            .width(max: 40)
+                            TableColumn("Bytes") { files in
+                                Text(files.transferredNumberSizebytes)
+                                    .frame(maxWidth: .infinity, alignment: .trailing)
+                            }
+                            .width(max: 60)
+                            TableColumn("Tot num") { files in
+                                Text(files.totalNumber)
+                                    .frame(maxWidth: .infinity, alignment: .trailing)
+                            }
+                            .width(max: 80)
+                            TableColumn("Tot bytes") { files in
+                                Text(files.totalNumberSizebytes)
+                                    .frame(maxWidth: .infinity, alignment: .trailing)
+                            }
+                            .width(max: 80)
+                            TableColumn("Tot dir") { files in
+                                Text(files.totalDirs)
+                                    .frame(maxWidth: .infinity, alignment: .trailing)
+                            }
+                            .width(max: 70)
                         }
-                        .width(max: 40)
-                        TableColumn("Bytes") { files in
-                            Text(files.transferredNumberSizebytes)
-                                .frame(maxWidth: .infinity, alignment: .trailing)
-                        }
-                        .width(max: 60)
-                        TableColumn("Tot num") { files in
-                            Text(files.totalNumber)
-                                .frame(maxWidth: .infinity, alignment: .trailing)
-                        }
-                        .width(max: 80)
-                        TableColumn("Tot bytes") { files in
-                            Text(files.totalNumberSizebytes)
-                                .frame(maxWidth: .infinity, alignment: .trailing)
-                        }
-                        .width(max: 80)
-                        TableColumn("Tot dir") { files in
-                            Text(files.totalDirs)
-                                .frame(maxWidth: .infinity, alignment: .trailing)
-                        }
-                        .width(max: 70)
+                        .foregroundColor(.blue)
+                        .frame(width: 450, height: 50, alignment: .center)
                     }
-                    .foregroundColor(.blue)
-                    .frame(width: 450, height: 50, alignment: .center)
 
                     List(outputfromrsync.output) { output in
                         Text(output.line)
@@ -116,8 +178,8 @@ struct DetailsView: View {
 extension DetailsView {
     func processtermination(data: [String]?) {
         outputfromrsync.generatedata(data)
-        gettingremotedata = false
         estimateddataonetask.update(data: data, hiddenID: selectedconfig?.hiddenID, config: selectedconfig)
+        gettingremotedata = false
     }
 }
 
