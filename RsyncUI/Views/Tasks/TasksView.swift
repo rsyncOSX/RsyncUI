5e6328fdbc5546d4b1caaa59df7f4fc68aa30f15
rsyncOSX <thomeven@gmail.com>
2023-05-30 13:58:49 +0200

    Update TasksView.swift

--- RsyncUI/Views/Tasks/TasksView.swift
+++ RsyncUI/Views/Tasks/TasksView.swift
@@ -63,7 +63,6 @@ struct TasksView: View {
                         }
                     } else {
                         selectedconfig.config = nil
-                        selecteduuids.removeAll()
                     }
                 },
                 inwork: $inwork,
