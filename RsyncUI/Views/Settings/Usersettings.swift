b75604225f8355b7a2634b95921c10c47dc5a38d
Thomas Evensen <thomeven@gmail.com>
2023-07-04 16:01:08 +0200

    update

--- RsyncUI/Views/Settings/Usersettings.swift
+++ RsyncUI/Views/Settings/Usersettings.swift
@@ -10,7 +10,7 @@ import SwiftUI
 
 struct Usersettings: View {
     @StateObject var rsyncversion = Rsyncversion()
-    @StateObject var usersettings = ObserveableUsersetting()
+    @State private var usersettings = ObserveableUsersetting()
     @State private var backup = false
 
     var body: some View {
@@ -26,8 +26,15 @@ struct Usersettings: View {
                     VStack(alignment: .leading) {
                         Section(header: headerrsync) {
                             HStack {
-                                ToggleViewDefault(NSLocalizedString("Rsync v3.x", comment: ""), $usersettings.rsyncversion3)
-                                ToggleViewDefault(NSLocalizedString("Apple Silicon", comment: ""), $usersettings.macosarm)
+                                ToggleViewDefault(NSLocalizedString("Rsync v3.x", comment: ""),
+                                                  $usersettings.rsyncversion3.onChange {
+                                                      SharedReference.shared.rsyncversion3 = usersettings.rsyncversion3
+
+                                                  })
+                                ToggleViewDefault(NSLocalizedString("Apple Silicon", comment: ""),
+                                                  $usersettings.macosarm.onChange {
+                                                      SharedReference.shared.macosarm = usersettings.macosarm
+                                                  })
                             }
                         }
 
@@ -52,36 +59,52 @@ struct Usersettings: View {
                         HStack {
                             VStack(alignment: .leading) {
                                 Section(header: headerloggingtofile) {
-                                    ToggleViewDefault(NSLocalizedString("None", comment: ""), $usersettings.nologging.onChange {
-                                        if usersettings.nologging == true {
-                                            usersettings.minimumlogging = false
-                                            usersettings.fulllogging = false
-                                        } else {
-                                            usersettings.minimumlogging = true
-                                            usersettings.fulllogging = false
-                                        }
-                                    })
+                                    ToggleViewDefault(NSLocalizedString("None", comment: ""),
+                                                      $usersettings.nologging.onChange {
+                                                          if usersettings.nologging == true {
+                                                              usersettings.minimumlogging = false
+                                                              usersettings.fulllogging = false
+                                                          } else {
+                                                              usersettings.minimumlogging = true
+                                                              usersettings.fulllogging = false
+                                                          }
+                                                          SharedReference.shared.fulllogging = usersettings.fulllogging
+                                                          SharedReference.shared.minimumlogging = usersettings.minimumlogging
+                                                          SharedReference.shared.nologging = usersettings.nologging
+                                                      })
 
-                                    ToggleViewDefault(NSLocalizedString("Min", comment: ""), $usersettings.minimumlogging.onChange {
-                                        if usersettings.minimumlogging == true {
-                                            usersettings.nologging = false
-                                            usersettings.fulllogging = false
-                                        }
-                                    })
+                                    ToggleViewDefault(NSLocalizedString("Min", comment: ""),
+                                                      $usersettings.minimumlogging.onChange {
+                                                          if usersettings.minimumlogging == true {
+                                                              usersettings.nologging = false
+                                                              usersettings.fulllogging = false
+                                                          }
+                                                          SharedReference.shared.fulllogging = usersettings.fulllogging
+                                                          SharedReference.shared.minimumlogging = usersettings.minimumlogging
+                                                          SharedReference.shared.nologging = usersettings.nologging
+                                                      })
 
-                                    ToggleViewDefault(NSLocalizedString("Full", comment: ""), $usersettings.fulllogging.onChange {
-                                        if usersettings.fulllogging == true {
-                                            usersettings.nologging = false
-                                            usersettings.minimumlogging = false
-                                        }
-                                    })
+                                    ToggleViewDefault(NSLocalizedString("Full", comment: ""),
+                                                      $usersettings.fulllogging.onChange {
+                                                          if usersettings.fulllogging == true {
+                                                              usersettings.nologging = false
+                                                              usersettings.minimumlogging = false
+                                                          }
+                                                          SharedReference.shared.fulllogging = usersettings.fulllogging
+                                                          SharedReference.shared.minimumlogging = usersettings.minimumlogging
+                                                          SharedReference.shared.nologging = usersettings.nologging
+                                                      })
                                 }
                             }
 
                             VStack(alignment: .leading) {
                                 Section(header: othersettings) {
-                                    ToggleViewDefault(NSLocalizedString("Detailed log level", comment: ""), $usersettings.detailedlogging)
-                                    ToggleViewDefault(NSLocalizedString("Monitor network", comment: ""), $usersettings.monitornetworkconnection)
+                                    ToggleViewDefault(NSLocalizedString("Detailed log level", comment: ""), $usersettings.detailedlogging.onChange {
+                                        SharedReference.shared.detailedlogging = usersettings.detailedlogging
+                                    })
+                                    ToggleViewDefault(NSLocalizedString("Monitor network", comment: ""), $usersettings.monitornetworkconnection.onChange {
+                                        SharedReference.shared.monitornetworkconnection = usersettings.monitornetworkconnection
+                                    })
                                 }
                             }
                         }
@@ -145,7 +168,10 @@ struct Usersettings: View {
     }
 
     var setrsyncpathdefault: some View {
-        EditValue(250, SetandValidatepathforrsync().getpathforrsync(), $usersettings.localrsyncpath)
+        EditValue(250, SetandValidatepathforrsync().getpathforrsync(),
+                  $usersettings.localrsyncpath.onChange {
+                      usersettings.setandvalidatepathforrsync(usersettings.localrsyncpath)
+                  })
     }
 
     // Restore path
@@ -154,12 +180,15 @@ struct Usersettings: View {
     }
 
     var setpathforrestore: some View {
-        EditValue(250, NSLocalizedString("Path for restore", comment: ""), $usersettings.temporarypathforrestore)
-            .onAppear(perform: {
-                if let pathforrestore = SharedReference.shared.pathforrestore {
-                    usersettings.temporarypathforrestore = pathforrestore
-                }
-            })
+        EditValue(250, NSLocalizedString("Path for restore", comment: ""),
+                  $usersettings.temporarypathforrestore.onChange {
+                      usersettings.setandvalidapathforrestore(usersettings.temporarypathforrestore)
+                  })
+                  .onAppear(perform: {
+                      if let pathforrestore = SharedReference.shared.pathforrestore {
+                          usersettings.temporarypathforrestore = pathforrestore
+                      }
+                  })
     }
 
     // Logging
@@ -184,10 +213,12 @@ struct Usersettings: View {
 
     var setmarkdays: some View {
         TextField("",
-                  text: $usersettings.marknumberofdayssince)
-            .textFieldStyle(RoundedBorderTextFieldStyle())
-            .frame(width: 70)
-            .lineLimit(1)
+                  text: $usersettings.marknumberofdayssince.onChange {
+                      usersettings.markdays(days: usersettings.marknumberofdayssince)
+                  })
+                  .textFieldStyle(RoundedBorderTextFieldStyle())
+                  .frame(width: 70)
+                  .lineLimit(1)
     }
 }
 
@@ -206,3 +237,5 @@ extension Usersettings {
         backup = true
     }
 }
+
+// swiftlint:enable line_length
