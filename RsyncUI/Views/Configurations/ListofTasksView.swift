e38208c566b234b629ef4df7257f5336cacb9668
Thomas Evensen <thomeven@gmail.com>
2023-07-16 08:55:01 +0200

    Update ListofTasksView.swift

--- RsyncUI/Views/Configurations/ListofTasksView.swift
+++ RsyncUI/Views/Configurations/ListofTasksView.swift
@@ -21,84 +21,11 @@ struct ListofTasksView: View {
 
     var body: some View {
         VStack {
-            if #available(macOS 13.0, *) {
-                tabledata
-            } else {
-                tabledata_macos12
-            }
+            tabledata
         }
         .searchable(text: $filterstring)
     }
 
-    var tabledata_macos12: some View {
-        Table(configurationssorted, selection: $selecteduuids) {
-            TableColumn("%") { data in
-                if data.hiddenID == inwork && progressdetails.isestimating() == false {
-                    ProgressView("",
-                                 value: progressdetails.getcurrentprogress(),
-                                 total: maxcount)
-                        .onChange(of: progressdetails.getcurrentprogress()) {}
-                        .frame(alignment: .center)
-                }
-            }
-            .width(max: 50)
-            TableColumn("Profile") { data in
-                if markconfig(data) {
-                    Text(data.profile ?? "Default profile")
-                        .foregroundColor(.red)
-                } else {
-                    Text(data.profile ?? "Default profile")
-                }
-            }
-            .width(min: 50, max: 200)
-            TableColumn("Synchronize ID", value: \.backupID)
-                .width(min: 50, max: 200)
-            TableColumn("Task", value: \.task)
-                .width(max: 80)
-            TableColumn("Local catalog", value: \.localCatalog)
-                .width(min: 80, max: 300)
-            TableColumn("Remote catalog", value: \.offsiteCatalog)
-                .width(min: 80, max: 300)
-            TableColumn("Server") { data in
-                if data.offsiteServer.count > 0 {
-                    Text(data.offsiteServer)
-                } else {
-                    Text("localhost")
-                }
-            }
-            .width(min: 50, max: 80)
-            TableColumn("Days") { data in
-                if markconfig(data) {
-                    Text(data.dayssincelastbackup ?? "")
-                        .foregroundColor(.red)
-                } else {
-                    Text(data.dayssincelastbackup ?? "")
-                }
-            }
-            .width(max: 50)
-            TableColumn("Last") { data in
-                if markconfig(data) {
-                    Text(data.dateRun ?? "")
-                        .foregroundColor(.red)
-                } else {
-                    Text(data.dateRun ?? "")
-                }
-            }
-            .width(max: 120)
-        }
-        .confirmationDialog(
-            NSLocalizedString("Delete configuration", comment: "")
-                + "?",
-            isPresented: $confirmdelete
-        ) {
-            Button("Delete") {
-                delete()
-                confirmdelete = false
-            }
-        }
-    }
-
-    @available(macOS 13.0, *)
     var tabledata: some View {
         Table(configurationssorted, selection: $selecteduuids) {
             TableColumn("%") { data in
