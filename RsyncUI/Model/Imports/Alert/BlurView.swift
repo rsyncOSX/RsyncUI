4a5672eb230df8fc07a9e0cd0192317eb6360145
Thomas Evensen <thomeven@gmail.com>
2023-09-23 08:38:43 +0200

    Update

--- RsyncUI/Model/Imports/Alert/BlurView.swift
+++ RsyncUI/Model/Imports/Alert/BlurView.swift
@@ -9,34 +9,34 @@ import Foundation
 import SwiftUI
 
 #if os(macOS)
-  public struct BlurView: NSViewRepresentable {
-    public typealias NSViewType = NSVisualEffectView
+    public struct BlurView: NSViewRepresentable {
+        public typealias NSViewType = NSVisualEffectView
 
-    public func makeNSView(context: Context) -> NSVisualEffectView {
-      let effectView = NSVisualEffectView()
-      effectView.material = .hudWindow
-      effectView.blendingMode = .withinWindow
-      effectView.state = NSVisualEffectView.State.active
-      return effectView
-    }
+        public func makeNSView(context _: Context) -> NSVisualEffectView {
+            let effectView = NSVisualEffectView()
+            effectView.material = .hudWindow
+            effectView.blendingMode = .withinWindow
+            effectView.state = NSVisualEffectView.State.active
+            return effectView
+        }
 
-    public func updateNSView(_ nsView: NSVisualEffectView, context: Context) {
-      nsView.material = .hudWindow
-      nsView.blendingMode = .withinWindow
+        public func updateNSView(_ nsView: NSVisualEffectView, context _: Context) {
+            nsView.material = .hudWindow
+            nsView.blendingMode = .withinWindow
+        }
     }
-  }
 
 #else
-  public struct BlurView: UIViewRepresentable {
-    public typealias UIViewType = UIVisualEffectView
+    public struct BlurView: UIViewRepresentable {
+        public typealias UIViewType = UIVisualEffectView
 
-    public func makeUIView(context: Context) -> UIVisualEffectView {
-      return UIVisualEffectView(effect: UIBlurEffect(style: .systemMaterial))
-    }
+        public func makeUIView(context _: Context) -> UIVisualEffectView {
+            return UIVisualEffectView(effect: UIBlurEffect(style: .systemMaterial))
+        }
 
-    public func updateUIView(_ uiView: UIVisualEffectView, context: Context) {
-      uiView.effect = UIBlurEffect(style: .systemMaterial)
+        public func updateUIView(_ uiView: UIVisualEffectView, context _: Context) {
+            uiView.effect = UIBlurEffect(style: .systemMaterial)
+        }
     }
-  }
 
 #endif
