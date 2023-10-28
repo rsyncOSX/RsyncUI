b6d811f4b2ca9a9af0f3a3f736d085d41ba33bb8
rsyncOSX <thomeven@gmail.com>
2023-10-28 15:00:09 +0200

    Update AlertToastModifier.swift

--- RsyncUI/Model/Imports/Alert/AlertToastModifier.swift
+++ RsyncUI/Model/Imports/Alert/AlertToastModifier.swift
@@ -123,8 +123,8 @@ public struct AlertToastModifier: ViewModifier {
                     }
                     .animation(Animation.spring(), value: isPresenting)
                 )
-                .onChange(of: isPresenting) { presented in
-                    if presented {
+                .onChange(of: isPresenting) {
+                    if isPresenting {
                         onAppearAction()
                     }
                 }
@@ -151,8 +151,8 @@ public struct AlertToastModifier: ViewModifier {
                         .offset(y: offset)
                         .animation(Animation.spring(), value: isPresenting))
                 )
-                .onChange(of: isPresenting) { presented in
-                    if presented {
+                .onChange(of: isPresenting) {
+                    if isPresenting {
                         onAppearAction()
                     }
                 }
@@ -167,8 +167,8 @@ public struct AlertToastModifier: ViewModifier {
                     .edgesIgnoringSafeArea(.all)
                     .animation(Animation.spring(), value: isPresenting)
                 )
-                .onChange(of: isPresenting) { presented in
-                    if presented {
+                .onChange(of: isPresenting) {
+                    if isPresenting {
                         onAppearAction()
                     }
                 }
