6008e492b615813b2a6c27c1b899b59a9903433f
Thomas Evensen <thomeven@gmail.com>
2023-12-04 09:11:18 +0100

    Update EstimateTasksAsync.swift

--- RsyncUI/Model/Execution/Async/EstimateTasksAsync.swift
+++ RsyncUI/Model/Execution/Async/EstimateTasksAsync.swift
@@ -7,7 +7,6 @@
 // swiftlint: disable line_length
 
 import Foundation
-import OSLog
 
 class EstimateTasksAsync {
     var structprofile: String?
@@ -66,7 +65,6 @@ class EstimateTasksAsync {
                 }
             }
         }
-        Logger.process.info("EstimateTasksAsync() : \(stackoftasktobeestimated?.count ?? 0, privacy: .public)")
         localestimateprogressdetails?.setmaxcount(stackoftasktobeestimated?.count ?? 0)
         localestimateprogressdetails?.setprofileandnumberofconfigurations(structprofile ?? "Default profile", localconfigurations?.getallconfigurations()?.count ?? 0)
     }
