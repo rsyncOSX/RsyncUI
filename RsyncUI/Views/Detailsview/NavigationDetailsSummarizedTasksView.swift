1c865cfc226a4764caf1e8075d8bb6891406e093
Thomas Evensen <thomeven@gmail.com>
2023-12-19 14:15:51 +0100

    Update NavigationDetailsSummarizedTasksView.swift

--- RsyncUI/Views/Detailsview/NavigationDetailsSummarizedTasksView.swift
+++ RsyncUI/Views/Detailsview/NavigationDetailsSummarizedTasksView.swift
@@ -22,133 +22,135 @@ struct NavigationSummarizedAllDetailsView: View {
     var body: some View {
         VStack {
             HStack {
-                if estimateprogressdetails.estimatealltasksasync {
-                    Text("Estimating")
-                } else {
-                    Table(estimateprogressdetails.getestimatedlist() ?? [],
-                          selection: $selecteduuids)
-                    {
-                        TableColumn("Synchronize ID") { data in
-                            if data.datatosynchronize {
-                                Text(data.backupID)
-                                    .foregroundColor(.blue)
-                            } else {
-                                Text(data.backupID)
-                            }
+                /*
+                 if estimateprogressdetails.estimatealltasksasync {
+                     Text("Estimating")
+                 } else {
+                     */
+                Table(estimateprogressdetails.getestimatedlist() ?? [],
+                      selection: $selecteduuids)
+                {
+                    TableColumn("Synchronize ID") { data in
+                        if data.datatosynchronize {
+                            Text(data.backupID)
+                                .foregroundColor(.blue)
+                        } else {
+                            Text(data.backupID)
                         }
-                        .width(min: 40, max: 80)
-                        TableColumn("Task", value: \.task)
-                            .width(max: 60)
-                        TableColumn("Local catalog", value: \.localCatalog)
-                            .width(min: 100, max: 300)
-                        TableColumn("Remote catalog", value: \.offsiteCatalog)
-                            .width(min: 100, max: 300)
-                        TableColumn("Server") { data in
-                            if data.offsiteServer.count > 0 {
-                                Text(data.offsiteServer)
-                            } else {
-                                Text("localhost")
-                            }
-                        }
-                        .width(max: 60)
                     }
+                    .width(min: 40, max: 80)
+                    TableColumn("Task", value: \.task)
+                        .width(max: 60)
+                    TableColumn("Local catalog", value: \.localCatalog)
+                        .width(min: 100, max: 300)
+                    TableColumn("Remote catalog", value: \.offsiteCatalog)
+                        .width(min: 100, max: 300)
+                    TableColumn("Server") { data in
+                        if data.offsiteServer.count > 0 {
+                            Text(data.offsiteServer)
+                        } else {
+                            Text("localhost")
+                        }
+                    }
+                    .width(max: 60)
+                }
 
-                    Table(estimateprogressdetails.getestimatedlist() ?? [],
-                          selection: $selecteduuids)
-                    {
-                        TableColumn("New") { files in
-                            if files.datatosynchronize {
-                                Text(files.newfiles)
-                                    .frame(maxWidth: .infinity, alignment: .trailing)
-                                    .foregroundColor(.blue)
-                            } else {
-                                Text(files.newfiles)
-                                    .frame(maxWidth: .infinity, alignment: .trailing)
-                            }
-                        }
-                        .width(max: 40)
-                        TableColumn("Delete") { files in
-                            if files.datatosynchronize {
-                                Text(files.deletefiles)
-                                    .frame(maxWidth: .infinity, alignment: .trailing)
-                                    .foregroundColor(.blue)
-                            } else {
-                                Text(files.deletefiles)
-                                    .frame(maxWidth: .infinity, alignment: .trailing)
-                            }
-                        }
-                        .width(max: 40)
-                        TableColumn("Files") { files in
-                            if files.datatosynchronize {
-                                Text(files.transferredNumber)
-                                    .frame(maxWidth: .infinity, alignment: .trailing)
-                                    .foregroundColor(.blue)
-                            } else {
-                                Text(files.transferredNumber)
-                                    .frame(maxWidth: .infinity, alignment: .trailing)
-                            }
-                        }
-                        .width(max: 40)
-                        TableColumn("Bytes") { files in
-                            if files.datatosynchronize {
-                                Text(files.transferredNumberSizebytes)
-                                    .frame(maxWidth: .infinity, alignment: .trailing)
-                                    .foregroundColor(.blue)
-                            } else {
-                                Text(files.transferredNumberSizebytes)
-                                    .frame(maxWidth: .infinity, alignment: .trailing)
-                            }
-                        }
-                        .width(max: 60)
-                        TableColumn("Tot num") { files in
-                            Text(files.totalNumber)
+                Table(estimateprogressdetails.getestimatedlist() ?? [],
+                      selection: $selecteduuids)
+                {
+                    TableColumn("New") { files in
+                        if files.datatosynchronize {
+                            Text(files.newfiles)
+                                .frame(maxWidth: .infinity, alignment: .trailing)
+                                .foregroundColor(.blue)
+                        } else {
+                            Text(files.newfiles)
                                 .frame(maxWidth: .infinity, alignment: .trailing)
                         }
-                        .width(max: 80)
-                        TableColumn("Tot bytes") { files in
-                            Text(files.totalNumberSizebytes)
-                                .frame(maxWidth: .infinity, alignment: .trailing)
-                        }
-                        .width(max: 80)
-                        TableColumn("Tot dir") { files in
-                            Text(files.totalDirs)
-                                .frame(maxWidth: .infinity, alignment: .trailing)
-                        }
-                        .width(max: 70)
                     }
-                    .onChange(of: selecteduuids) {
-                        guard selecteduuids.count > 0 else { return }
-                        path.append(Tasks(task: .dryrunonetaskalreadyestimated))
+                    .width(max: 40)
+                    TableColumn("Delete") { files in
+                        if files.datatosynchronize {
+                            Text(files.deletefiles)
+                                .frame(maxWidth: .infinity, alignment: .trailing)
+                                .foregroundColor(.blue)
+                        } else {
+                            Text(files.deletefiles)
+                                .frame(maxWidth: .infinity, alignment: .trailing)
+                        }
                     }
+                    .width(max: 40)
+                    TableColumn("Files") { files in
+                        if files.datatosynchronize {
+                            Text(files.transferredNumber)
+                                .frame(maxWidth: .infinity, alignment: .trailing)
+                                .foregroundColor(.blue)
+                        } else {
+                            Text(files.transferredNumber)
+                                .frame(maxWidth: .infinity, alignment: .trailing)
+                        }
+                    }
+                    .width(max: 40)
+                    TableColumn("Bytes") { files in
+                        if files.datatosynchronize {
+                            Text(files.transferredNumberSizebytes)
+                                .frame(maxWidth: .infinity, alignment: .trailing)
+                                .foregroundColor(.blue)
+                        } else {
+                            Text(files.transferredNumberSizebytes)
+                                .frame(maxWidth: .infinity, alignment: .trailing)
+                        }
+                    }
+                    .width(max: 60)
+                    TableColumn("Tot num") { files in
+                        Text(files.totalNumber)
+                            .frame(maxWidth: .infinity, alignment: .trailing)
+                    }
+                    .width(max: 80)
+                    TableColumn("Tot bytes") { files in
+                        Text(files.totalNumberSizebytes)
+                            .frame(maxWidth: .infinity, alignment: .trailing)
+                    }
+                    .width(max: 80)
+                    TableColumn("Tot dir") { files in
+                        Text(files.totalDirs)
+                            .frame(maxWidth: .infinity, alignment: .trailing)
+                    }
+                    .width(max: 70)
+                }
+                .onChange(of: selecteduuids) {
+                    guard selecteduuids.count > 0 else { return }
+                    path.append(Tasks(task: .dryrunonetaskalreadyestimated))
                 }
             }
-            .toolbar(content: {
-                ToolbarItem {
-                    Button {
-                        path.removeAll()
-                        path.append(Tasks(task: .executestimatedview))
-                    } label: {
-                        Image(systemName: "arrowshape.turn.up.left.fill")
-                            .foregroundColor(Color(.blue))
-                    }
-                    .help("Synchronize (⌘R)")
-                }
-            })
-            .focusedSceneValue(\.startexecution, $focusstartexecution)
-            .onAppear {
-                guard estimateprogressdetails.estimatealltasksasync == false else {
-                    Logger.process.warning("TasksView: estimate already in progress")
-                    return
-                }
-                estimateprogressdetails.resetcounts()
-                executeprogressdetails.resetcounts()
-                estimateprogressdetails.startestimateasync()
-            }
-
-            Spacer()
-
-            if nodatatosynchronize { shownosynchronize }
         }
+        .toolbar(content: {
+            ToolbarItem {
+                Button {
+                    path.removeAll()
+                    path.append(Tasks(task: .executestimatedview))
+                } label: {
+                    Image(systemName: "arrowshape.turn.up.left.fill")
+                        .foregroundColor(Color(.blue))
+                }
+                .help("Synchronize (⌘R)")
+            }
+        })
+        .focusedSceneValue(\.startexecution, $focusstartexecution)
+        .onAppear {
+            guard estimateprogressdetails.estimatealltasksasync == false else {
+                Logger.process.warning("TasksView: estimate already in progress")
+                return
+            }
+            estimateprogressdetails.resetcounts()
+            executeprogressdetails.resetcounts()
+            estimateprogressdetails.startestimateasync()
+        }
+
+        Spacer()
+
+        if nodatatosynchronize { shownosynchronize }
+        // }
 
         if estimateprogressdetails.estimatealltasksasync { progressviewestimateasync }
 
