b6b5cf6165933df48dd37e5a52e820ff3a50dd8a
Thomas Evensen <thomeven@gmail.com>
2023-12-20 08:10:14 +0100

    AlertError

---
+++ RsyncUI/Model/Global/AlertError.swift
@@ -0,0 +1,49 @@
+//
+//  AlertError.swift
+//  RsyncUI
+//
+//  Created by Thomas Evensen on 20/12/2023.
+//
+
+import Foundation
+import Observation
+import SwiftUI
+
+@Observable
+final class AlertError {
+    private(set) var activeError: Error?
+
+    func alert(error: Error) {
+        DispatchQueue.main.async {
+            self.activeError = error
+        }
+    }
+
+    var presentalert: Binding<Bool> {
+        return Binding<Bool>(
+            get: { self.activeError != nil },
+            set: { value in
+                guard !value else { return }
+                self.activeError = nil
+            }
+        )
+    }
+}
+
+extension Alert {
+    init(localizedError: Error) {
+        self = Alert(nsError: localizedError as NSError)
+    }
+
+    init(nsError: NSError) {
+        let message: Text? = {
+            let message = [nsError.localizedFailureReason,
+                           nsError.localizedRecoverySuggestion]
+                .compactMap { $0 }.joined(separator: "\n\n")
+            return message.isEmpty ? nil : Text(message)
+        }()
+        self = Alert(title: Text(nsError.localizedDescription),
+                     message: message,
+                     dismissButton: .default(Text("OK")))
+    }
+}
