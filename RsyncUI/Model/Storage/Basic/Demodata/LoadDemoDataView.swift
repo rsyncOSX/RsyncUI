42ed097b333302ea49502b7f480ccf8e48ce8d63
Thomas Evensen <thomeven@gmail.com>
2024-01-22 14:26:45 +0100

    Update LoadDemoDataView.swift

--- RsyncUI/Model/Storage/Basic/Demodata/LoadDemoDataView.swift
+++ RsyncUI/Model/Storage/Basic/Demodata/LoadDemoDataView.swift
@@ -1,5 +1,5 @@
 //
-//  LoadTestDataView.swift
+//  LoadDemoDataView.swift
 //  RsyncUI
 //
 //  Created by Thomas Evensen on 22/01/2024.
@@ -9,11 +9,11 @@ import SwiftUI
 
 struct LoadTestDataView: View {
     @Bindable var rsyncUIdata: RsyncUIconfigurations
-    
+
     var body: some View {
         Text("")
     }
-    
+
     func verifyloaddemodata() -> Bool {
         return rsyncUIdata.configurations?.count == 0
     }
