aa6066b2addcf607d7c8ac7b9c01783f84b7b4f7
Thomas Evensen <thomeven@gmail.com>
2024-02-12 14:14:44 +0100

    Update

--- RsyncUI/Model/Data/RsyncUIlogrecords.swift
+++ RsyncUI/Model/Data/RsyncUIlogrecords.swift
@@ -14,54 +14,6 @@ final class RsyncUIlogrecords {
     var profile: String?
     var logrecords: [LogRecords]?
 
-    @ObservationIgnored var countrecords: Int = 0
-    /*
-        func filterlogs(_ filter: String, _ hiddenID: Int) -> [Log] {
-            var activelogrecords: [Log]?
-            switch hiddenID {
-            case -1:
-                if filter.isEmpty == false {
-                    activelogrecords = nil
-                    activelogrecords = alllogssorted?.filter {
-                        $0.dateExecuted?.en_us_date_from_string().long_localized_string_from_date().contains(filter) ?? false ||
-                            $0.resultExecuted?.contains(filter) ?? false
-                    }
-                    let number = activelogrecords?.count ?? 0
-                    Logger.process.info("filter ALL logs by \(filter, privacy: .public) - count: \(String(number), privacy: .public)")
-                    countrecords = number
-                    return activelogrecords ?? []
-
-                } else {
-                    let number = alllogssorted?.count ?? 0
-                    Logger.process.info("ALL logs - count: \(String(number), privacy: .public)")
-                    countrecords = number
-                    return alllogssorted ?? []
-                }
-            default:
-                if filter.isEmpty == false {
-                    activelogrecords = alllogssorted?.filter { $0.hiddenID == hiddenID }.sorted(by: \.date, using: >).filter {
-                        $0.dateExecuted?.en_us_date_from_string().long_localized_string_from_date().contains(filter) ?? false ||
-                            $0.resultExecuted?.contains(filter) ?? false
-                    }
-                    let number = activelogrecords?.count ?? 0
-                    Logger.process.info("filter logs BY hiddenID and filter by \(filter) - count: \(String(number), privacy: .public)")
-                    countrecords = number
-                    return activelogrecords ?? []
-                } else {
-                    activelogrecords = nil
-                    activelogrecords = alllogssorted?.filter { $0.hiddenID == hiddenID }.sorted(by: \.date, using: >)
-                    let number = activelogrecords?.count ?? 0
-                    Logger.process.info("filter logs BY hiddenID - count: \(String(number), privacy: .public)")
-                    countrecords = number
-                    return activelogrecords ?? []
-                }
-            }
-        }
-
-        func removerecords(_ uuids: Set<UUID>) {
-            alllogssorted?.removeAll(where: { uuids.contains($0.id) })
-        }
-     */
     init(_ profile: String?,
          _ logrecordsfromstore: [LogRecords]?)
     {
--- RsyncUI/Views/LogRecords/LogsbyConfigurationView.swift
+++ RsyncUI/Views/LogRecords/LogsbyConfigurationView.swift
@@ -77,7 +77,7 @@ struct LogsbyConfigurationView: View {
                 if showindebounce {
                     indebounce
                 } else {
-                    Text("\(rsyncUIlogrecords.countrecords)")
+                    Text("\(logs.count)")
                 }
                 Spacer()
             }
@@ -122,6 +122,7 @@ struct LogsbyConfigurationView: View {
             .controlSize(.small)
     }
 
+    // TODO: fix filter and by hiddenID
     var logs: [Log] {
         if let logrecords = rsyncUIlogrecords.logrecords {
             if hiddenID == -1 {
@@ -136,3 +137,51 @@ struct LogsbyConfigurationView: View {
         return []
     }
 }
+
+/*
+    func filterlogs(_ filter: String, _ hiddenID: Int) -> [Log] {
+        var activelogrecords: [Log]?
+        switch hiddenID {
+        case -1:
+            if filter.isEmpty == false {
+                activelogrecords = nil
+                activelogrecords = alllogssorted?.filter {
+                    $0.dateExecuted?.en_us_date_from_string().long_localized_string_from_date().contains(filter) ?? false ||
+                        $0.resultExecuted?.contains(filter) ?? false
+                }
+                let number = activelogrecords?.count ?? 0
+                Logger.process.info("filter ALL logs by \(filter, privacy: .public) - count: \(String(number), privacy: .public)")
+                countrecords = number
+                return activelogrecords ?? []
+
+            } else {
+                let number = alllogssorted?.count ?? 0
+                Logger.process.info("ALL logs - count: \(String(number), privacy: .public)")
+                countrecords = number
+                return alllogssorted ?? []
+            }
+        default:
+            if filter.isEmpty == false {
+                activelogrecords = alllogssorted?.filter { $0.hiddenID == hiddenID }.sorted(by: \.date, using: >).filter {
+                    $0.dateExecuted?.en_us_date_from_string().long_localized_string_from_date().contains(filter) ?? false ||
+                        $0.resultExecuted?.contains(filter) ?? false
+                }
+                let number = activelogrecords?.count ?? 0
+                Logger.process.info("filter logs BY hiddenID and filter by \(filter) - count: \(String(number), privacy: .public)")
+                countrecords = number
+                return activelogrecords ?? []
+            } else {
+                activelogrecords = nil
+                activelogrecords = alllogssorted?.filter { $0.hiddenID == hiddenID }.sorted(by: \.date, using: >)
+                let number = activelogrecords?.count ?? 0
+                Logger.process.info("filter logs BY hiddenID - count: \(String(number), privacy: .public)")
+                countrecords = number
+                return activelogrecords ?? []
+            }
+        }
+    }
+
+    func removerecords(_ uuids: Set<UUID>) {
+        alllogssorted?.removeAll(where: { uuids.contains($0.id) })
+    }
+ */
