aa9d113d704586623e87a5c0ee4a67998d052e45
Thomas Evensen <thomeven@gmail.com>
2024-03-18 17:06:58 +0100

    Update ExecuteProgressDetails.swift

--- RsyncUI/Model/Execution/ProgressCounts/ExecuteProgressDetails.swift
+++ RsyncUI/Model/Execution/ProgressCounts/ExecuteProgressDetails.swift
@@ -24,9 +24,9 @@ final class ExecuteProgressDetails {
     func getmaxcountbytask() -> Double {
         let max = estimatedlist?.filter { $0.hiddenID == hiddenIDatwork }
         if (max?.count ?? 0) == 1 {
-            let num = Double(max?[0].outputfromrsync?.count ?? 0)
+            let num = Double(max?[0].outputfromrsync?.count ?? 0) + 3
             Logger.process.info("ExecuteProgressDetails (getmaxcount): \(num, privacy: .public)")
-            return Double(max?[0].outputfromrsync?.count ?? 0)
+            return Double(max?[0].outputfromrsync?.count ?? 0) + 3
         } else {
             return 0
         }
