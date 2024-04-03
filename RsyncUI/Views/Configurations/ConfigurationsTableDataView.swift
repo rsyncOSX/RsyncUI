88d135ad97486f17b85640e23781513eb1c2b8bb
Thomas Evensen <thomeven@gmail.com>
2024-04-03 08:05:10 +0200

    Update

---
+++ RsyncUI/Views/Configurations/ConfigurationsTableDataView.swift
@@ -0,0 +1,60 @@
+//
+//  TableDataView.swift
+//  RsyncUI
+//
+//  Created by Thomas Evensen on 03/04/2024.
+//
+
+import SwiftUI
+
+struct ConfigurationsTableDataView: View {
+    @Binding var selecteduuids: Set<SynchronizeConfiguration.ID>
+    @Binding var filterstring: String
+    let profile: String?
+    let configurations: [SynchronizeConfiguration]
+    
+    
+    var body: some View {
+        Table(configurations.filter {
+            filterstring.isEmpty ? true : $0.backupID.contains(filterstring)
+        }, selection: $selecteduuids) {
+            TableColumn("Profile") { _ in
+                Text(profile ?? "Default profile")
+            }
+            .width(min: 50, max: 200)
+            TableColumn("Synchronize ID", value: \.backupID)
+                .width(min: 50, max: 200)
+            TableColumn("Task", value: \.task)
+                .width(max: 80)
+            TableColumn("Local catalog", value: \.localCatalog)
+                .width(min: 80, max: 300)
+            TableColumn("Remote catalog", value: \.offsiteCatalog)
+                .width(min: 80, max: 300)
+            TableColumn("Server") { data in
+                if data.offsiteServer.count > 0 {
+                    Text(data.offsiteServer)
+                } else {
+                    Text("localhost")
+                }
+            }
+            .width(min: 50, max: 90)
+            TableColumn("Days") { data in
+                var seconds: Double {
+                    if let date = data.dateRun {
+                        let lastbackup = date.en_us_date_from_string()
+                        return lastbackup.timeIntervalSinceNow * -1
+                    } else {
+                        return 0
+                    }
+                }
+                Text(String(format: "%.2f", seconds / (60 * 60 * 24)))
+            }
+            .width(max: 50)
+            TableColumn("Last") { data in
+                Text(data.dateRun ?? "")
+            }
+            .width(max: 120)
+        }
+    }
+}
+
