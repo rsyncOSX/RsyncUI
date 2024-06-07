487a853597a5438ac52647e55d7d96a1a95f3a7f
Thomas Evensen <thomeven@gmail.com>
2024-06-07 18:18:52 +0200

    Update

---
+++ RsyncUI/Views/OutputViews/DetailsView.swift
@@ -0,0 +1,126 @@
+//
+//  DetailsView.swift
+//  RsyncUI
+//
+//  Created by Thomas Evensen on 07/06/2024.
+//
+
+import SwiftUI
+
+struct DetailsView: View {
+    let estimatedtask: RemoteDataNumbers
+    let outputfromrsync: ObservableOutputfromrsync
+
+    var body: some View {
+        ZStack {
+            HStack {
+                VStack(alignment: .leading) {
+                    VStack(alignment: .leading) {
+                        LabeledContent("Synchronize ID: ") {
+                            if estimatedtask.backupID.count == 0 {
+                                Text("Synchronize ID")
+                                    .foregroundColor(.blue)
+                            } else {
+                                Text(estimatedtask.backupID)
+                                    .foregroundColor(.blue)
+                            }
+                        }
+                        .padding(-3)
+
+                        LabeledContent("Task: ") {
+                            Text(estimatedtask.task)
+                                .foregroundColor(.blue)
+                        }
+                        .padding(-3)
+
+                        LabeledContent("Local catalog: ") {
+                            Text(estimatedtask.localCatalog)
+                                .foregroundColor(.blue)
+                        }
+                        .padding(-3)
+
+                        LabeledContent("Remote catalog: ") {
+                            Text(estimatedtask.offsiteCatalog)
+                                .foregroundColor(.blue)
+                        }
+                        .padding(-3)
+
+                        LabeledContent("Server: ") {
+                            if estimatedtask.offsiteServer.count == 0 {
+                                Text("localhost")
+                                    .foregroundColor(.blue)
+                            } else {
+                                Text(estimatedtask.offsiteServer)
+                                    .foregroundColor(.blue)
+                            }
+                        }
+                        .padding(-3)
+                    }
+                    .padding()
+
+                    VStack(alignment: .leading) {
+                        LabeledContent("Total number of files: ") {
+                            Text(estimatedtask.totalNumber)
+                                .foregroundColor(.blue)
+                        }
+                        .padding(-3)
+
+                        LabeledContent("Total number of catalogs: ") {
+                            Text(estimatedtask.totalDirs)
+                                .foregroundColor(.blue)
+                        }
+                        .padding(-3)
+
+                        LabeledContent("Total numbers: ") {
+                            Text(estimatedtask.totalNumber_totalDirs)
+                                .foregroundColor(.blue)
+                        }
+                        .padding(-3)
+
+                        LabeledContent("Total bytes: ") {
+                            Text(estimatedtask.totalNumberSizebytes)
+                                .foregroundColor(.blue)
+                        }
+                        .padding(-3)
+                    }
+                    .padding()
+
+                    Spacer()
+
+                    if estimatedtask.datatosynchronize {
+                        VStack(alignment: .leading) {
+                            Text("^[\(estimatedtask.newfiles_Int) file](inflect: true) new")
+                            Text("^[\(estimatedtask.deletefiles_Int) file](inflect: true) for delete")
+                            Text("^[\(estimatedtask.transferredNumber_Int) file](inflect: true) changed")
+                            Text("^[\(estimatedtask.transferredNumberSizebytes_Int) byte](inflect: true) for transfer")
+                        }
+                        .padding()
+                        .foregroundStyle(.white)
+                        .background {
+                            RoundedRectangle(cornerRadius: 8)
+                                .fill(.blue.gradient)
+                        }
+                        .padding()
+
+                    } else {
+                        Text("No data to synchronize")
+                            .font(.title2)
+                            .padding()
+                            .foregroundStyle(.white)
+                            .background {
+                                RoundedRectangle(cornerRadius: 8)
+                                    .fill(.blue.gradient)
+                            }
+                            .padding()
+                    }
+                }
+
+                Table(outputfromrsync.output) {
+                    TableColumn("Output from rsync") { data in
+                        Text(data.line)
+                    }
+                }
+            }
+        }
+    }
+}
--- RsyncUI/Views/OutputViews/OutputRsyncVerifyView.swift
+++ RsyncUI/Views/OutputViews/OutputRsyncVerifyView.swift
@@ -9,128 +9,22 @@ import SwiftUI
 
 struct OutputRsyncVerifyView: View {
     @State private var outputfromrsync = ObservableOutputfromrsync()
-    @State private var progress = false
-    @State private var data: [String]?
+    @State private var progress = true
+    @State private var estimatedtask: RemoteDataNumbers?
 
     let config: SynchronizeConfiguration
 
     var body: some View {
         ZStack {
-            HStack {
-                VStack(alignment: .leading) {
-                    VStack(alignment: .leading) {
-                        LabeledContent("Synchronize ID: ") {
-                            if estimatedtask.backupID.count == 0 {
-                                Text("Synchronize ID")
-                                    .foregroundColor(.blue)
-                            } else {
-                                Text(estimatedtask.backupID)
-                                    .foregroundColor(.blue)
-                            }
-                        }
-                        .padding(-3)
-
-                        LabeledContent("Task: ") {
-                            Text(estimatedtask.task)
-                                .foregroundColor(.blue)
-                        }
-                        .padding(-3)
-
-                        LabeledContent("Local catalog: ") {
-                            Text(estimatedtask.localCatalog)
-                                .foregroundColor(.blue)
-                        }
-                        .padding(-3)
-
-                        LabeledContent("Remote catalog: ") {
-                            Text(estimatedtask.offsiteCatalog)
-                                .foregroundColor(.blue)
-                        }
-                        .padding(-3)
-
-                        LabeledContent("Server: ") {
-                            if estimatedtask.offsiteServer.count == 0 {
-                                Text("localhost")
-                                    .foregroundColor(.blue)
-                            } else {
-                                Text(estimatedtask.offsiteServer)
-                                    .foregroundColor(.blue)
-                            }
-                        }
-                        .padding(-3)
-                    }
-                    .padding()
-
-                    VStack(alignment: .leading) {
-                        LabeledContent("Total number of files: ") {
-                            Text(estimatedtask.totalNumber)
-                                .foregroundColor(.blue)
-                        }
-                        .padding(-3)
-
-                        LabeledContent("Total number of catalogs: ") {
-                            Text(estimatedtask.totalDirs)
-                                .foregroundColor(.blue)
-                        }
-                        .padding(-3)
-
-                        LabeledContent("Total numbers: ") {
-                            Text(estimatedtask.totalNumber_totalDirs)
-                                .foregroundColor(.blue)
-                        }
-                        .padding(-3)
-
-                        LabeledContent("Total bytes: ") {
-                            Text(estimatedtask.totalNumberSizebytes)
-                                .foregroundColor(.blue)
-                        }
-                        .padding(-3)
-                    }
-                    .padding()
-
-                    Spacer()
-
-                    if estimatedtask.datatosynchronize {
-                        VStack(alignment: .leading) {
-                            Text("^[\(estimatedtask.newfiles_Int) file](inflect: true) new")
-                            Text("^[\(estimatedtask.deletefiles_Int) file](inflect: true) for delete")
-                            Text("^[\(estimatedtask.transferredNumber_Int) file](inflect: true) changed")
-                            Text("^[\(estimatedtask.transferredNumberSizebytes_Int) byte](inflect: true) for transfer")
-                        }
-                        .padding()
-                        .foregroundStyle(.white)
-                        .background {
-                            RoundedRectangle(cornerRadius: 8)
-                                .fill(.blue.gradient)
-                        }
-                        .padding()
-
-                    } else {
-                        Text("No data to synchronize")
-                            .font(.title2)
-                            .padding()
-                            .foregroundStyle(.white)
-                            .background {
-                                RoundedRectangle(cornerRadius: 8)
-                                    .fill(.blue.gradient)
-                            }
-                            .padding()
-                    }
-                }
-
-                Table(outputfromrsync.output) {
-                    TableColumn("Output from rsync") { data in
-                        Text(data.line)
-                    }
-                }
-            }
-
             if progress {
                 ProgressView()
+            } else {
+                if let estimatedtask = estimatedtask {
+                    DetailsView(estimatedtask: estimatedtask, outputfromrsync: outputfromrsync)
+                }
             }
         }
         .onAppear {
-            progress = true
             verify(config: config)
         }
         .toolbar(content: {
@@ -145,11 +39,6 @@ struct OutputRsyncVerifyView: View {
         })
     }
 
-    var estimatedtask: RemoteDataNumbers {
-        return RemoteDataNumbers(outputfromrsync: data,
-                                 config: config)
-    }
-
     func verify(config: SynchronizeConfiguration) {
         var arguments: [String]?
         arguments = ArgumentsSynchronize(config: config).argumentssynchronize(dryRun: true,
@@ -158,15 +47,16 @@ struct OutputRsyncVerifyView: View {
                                                 config: config,
                                                 processtermination: processtermination)
         process.executeProcess()
+    }
 
-        func processtermination(data: [String]?, hiddenID _: Int?) {
-            progress = false
-            outputfromrsync.generateoutput(data)
-            self.data = data
-        }
+    func processtermination(data: [String]?, hiddenID _: Int?) {
+        progress = false
+        outputfromrsync.generateoutput(data)
+        estimatedtask = RemoteDataNumbers(outputfromrsync: data,
+                                          config: config)
+    }
 
-        func abort() {
-            _ = InterruptProcess()
-        }
+    func abort() {
+        _ = InterruptProcess()
     }
 }
