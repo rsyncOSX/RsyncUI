02ca1a5b950c4bb808f1f34c3cf24b8ddcba6877
Thomas Evensen <thomeven@gmail.com>
2024-08-27 14:14:09 +0200

    Update CompletedView.swift

--- RsyncUI/Views/Tasks/CompletedView.swift
+++ RsyncUI/Views/Tasks/CompletedView.swift
@@ -11,13 +11,9 @@ struct CompletedView: View {
     @Binding var path: [Tasks]
 
     var body: some View {
-        Text("Synchronize data is completed")
-            .font(.title2)
-            .onAppear(perform: {
-                Task {
-                    try await Task.sleep(seconds: 0.5)
-                    path.removeAll()
-                }
-            })
+        MessageView(dismissafter: 1, mytext: "Synchronize data is completed", width: 450)
+            .onDisappear {
+                path.removeAll()
+            }
     }
 }
