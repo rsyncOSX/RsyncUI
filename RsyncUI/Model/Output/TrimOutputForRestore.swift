360a0f0119723ca221a6f8b0e3a48beb2f209732
Thomas Evensen <thomeven@gmail.com>
2024-10-22 19:24:48 +0200

    Update TrimOutputForRestore.swift

--- RsyncUI/Model/Output/TrimOutputForRestore.swift
+++ RsyncUI/Model/Output/TrimOutputForRestore.swift
@@ -12,19 +12,13 @@ final class TrimOutputForRestore: PropogateError {
     var trimmeddata = [String]()
 
     init(_ stringoutputfromrsync: [String]) {
-        trimmeddata = stringoutputfromrsync.map { line in
+        trimmeddata = stringoutputfromrsync.compactMap { line in
             let substr = line.dropFirst(10).trimmingCharacters(in: .whitespacesAndNewlines)
             let str = substr.components(separatedBy: " ").dropFirst(3).joined(separator: " ")
-            if str.isEmpty == false,
-               str.contains(".DS_Store") == false,
-               str.contains("bytes") == false,
-               str.contains("speedup") == false
-            {
-                var augmentetline = ""
-                augmentetline.append("./" + str)
-                return augmentetline
-            }
-            return str
+            return (str.isEmpty == false &&
+                    str.contains(".DS_Store") == false &&
+                    str.contains("bytes") == false &&
+                    str.contains("speedup") == false) ? ("./" + str) : nil
         }
     }
 }
