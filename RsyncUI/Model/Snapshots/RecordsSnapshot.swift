77a617efb23090dd41f0bdeb0a15831e389fdf43
Thomas Evensen <thomeven@gmail.com>
2024-10-25 07:11:18 +0200

    update

--- RsyncUI/Model/Snapshots/RecordsSnapshot.swift
+++ RsyncUI/Model/Snapshots/RecordsSnapshot.swift
@@ -9,56 +9,31 @@ import Foundation
 
 final class RecordsSnapshot {
     var loggrecordssnapshots: [LogRecordSnapshot]?
-    /*
-       private func readandsortallloggdata(_ config: SynchronizeConfiguration,
-                                           _ logrecords: [LogRecords])
-       {
-           var data = [LogRecordSnapshot]()
-           let localrecords = logrecords.filter { $0.hiddenID == config.hiddenID }
-           guard localrecords.count == 1 else { return }
-           for i in 0 ..< (localrecords[0].logrecords?.count ?? 0) {
-               var datestring: String?
-               var date: Date?
-               if let stringdate = localrecords[0].logrecords?[i].dateExecuted {
-                   if stringdate.isEmpty == false {
-                       datestring = stringdate.en_us_date_from_string().localized_string_from_date()
-                       date = stringdate.en_us_date_from_string()
-                   }
-               }
-               let record =
-                   LogRecordSnapshot(
-                       date: date ?? Date(),
-                       dateExecuted: datestring ?? "",
-                       resultExecuted: localrecords[0].logrecords?[i].resultExecuted ?? ""
-                   )
-               data.append(record)
-           }
-           loggrecordssnapshots = data.sorted(by: \.date, using: >)
-       }
-     */
+    
     private func readandsortallloggdata(_ config: SynchronizeConfiguration,
                                         _ logrecords: [LogRecords])
     {
         var data: [LogRecordSnapshot]?
         let localrecords = logrecords.filter { $0.hiddenID == config.hiddenID }
         guard localrecords.count == 1 else { return }
-        let logrecords = localrecords[0].logrecords ?? []
-        data = logrecords.map { record in
-            var datestring: String?
-            var date: Date?
-            if let stringdate = record.dateExecuted {
-                if stringdate.isEmpty == false {
-                    datestring = stringdate.en_us_date_from_string().localized_string_from_date()
-                    date = stringdate.en_us_date_from_string()
+        if let logrecords = localrecords[0].logrecords {
+            data = logrecords.map { record in
+                var datestring: String?
+                var date: Date?
+                if let stringdate = record.dateExecuted {
+                    if stringdate.isEmpty == false {
+                        datestring = stringdate.en_us_date_from_string().localized_string_from_date()
+                        date = stringdate.en_us_date_from_string()
+                    }
                 }
+                return LogRecordSnapshot(
+                    date: date ?? Date(),
+                    dateExecuted: datestring ?? "",
+                    resultExecuted: record.resultExecuted ?? ""
+                )
             }
-            return LogRecordSnapshot(
-                date: date ?? Date(),
-                dateExecuted: datestring ?? "",
-                resultExecuted: record.resultExecuted ?? ""
-            )
+            loggrecordssnapshots = data?.sorted(by: \.date, using: >)
         }
-        loggrecordssnapshots = data?.sorted(by: \.date, using: >)
     }
 
     init(config: SynchronizeConfiguration,
