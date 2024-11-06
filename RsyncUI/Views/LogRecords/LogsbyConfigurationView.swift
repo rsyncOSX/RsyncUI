d6ab255cadff3440aabcd12c796002996f9d11d8
Thomas Evensen <thomeven@gmail.com>
2024-11-06 16:17:03 +0100

    Update LogsbyConfigurationView.swift

--- RsyncUI/Views/LogRecords/LogsbyConfigurationView.swift
+++ RsyncUI/Views/LogRecords/LogsbyConfigurationView.swift
@@ -151,13 +151,13 @@ struct LogsbyConfigurationView: View {
                         merged += [logrecords].flatMap { $0 }
                     }
                 }
-                // return merged.sorted(by: \.date, using: >)
                 let records = merged.sorted(using: [KeyPathComparator(\Log.date, order: .reverse)])
                 logs = records.filter { ($0.dateExecuted?.en_us_date_from_string().long_localized_string_from_date().contains(filterstring)) ?? false || ($0.resultExecuted?.contains(filterstring) ?? false)
                 }
             } else {
-                if let index = logrecords.firstIndex(where: { $0.hiddenID == hiddenID }) {
-                    let records = (logrecords[index].logrecords ?? []).sorted(using: [KeyPathComparator(\Log.date, order: .reverse)])
+                if let index = logrecords.firstIndex(where: { $0.hiddenID == hiddenID }),
+                   let logrecords = logrecords[index].logrecords {
+                    let records = logrecords.sorted(using: [KeyPathComparator(\Log.date, order: .reverse)])
                     logs = records.filter { ($0.dateExecuted?.en_us_date_from_string().long_localized_string_from_date().contains(filterstring)) ?? false || ($0.resultExecuted?.contains(filterstring) ?? false)
                     }
                 }
@@ -177,8 +177,9 @@ struct LogsbyConfigurationView: View {
                 // return merged.sorted(by: \.date, using: >)
                 logs = merged.sorted(using: [KeyPathComparator(\Log.date, order: .reverse)])
             } else {
-                if let index = logrecords.firstIndex(where: { $0.hiddenID == hiddenID }) {
-                    logs = (logrecords[index].logrecords ?? []).sorted(using: [KeyPathComparator(\Log.date, order: .reverse)])
+                if let index = logrecords.firstIndex(where: { $0.hiddenID == hiddenID }),
+                    let logrecords = logrecords[index].logrecords {
+                    logs = logrecords.sorted(using: [KeyPathComparator(\Log.date, order: .reverse)])
                 }
             }
         }
