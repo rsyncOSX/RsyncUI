773cc24815c7b5c64c71e921ceeae987d2eafb7b
Thomas Evensen <thomeven@gmail.com>
2025-01-10 09:56:08 +0100

    update

---
+++ RsyncUIWidget/RsyncUIWidgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  RsyncUIWidgetBundle.swift
+//  RsyncUIWidget
+//
+//  Created by Thomas Evensen on 10/01/2025.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct RsyncUIWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        RsyncUIWidget()
+    }
+}
