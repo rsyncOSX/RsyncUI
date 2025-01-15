cbfa603de3bac787795338b1e4b262458c71107e
Thomas Evensen <thomeven@gmail.com>
2025-01-15 08:57:06 +0100

    update

---
+++ WidgetEstimate/WidgetEstimateBundle.swift
@@ -0,0 +1,16 @@
+//
+//  WidgetEstimateBundle.swift
+//  WidgetEstimate
+//
+//  Created by Thomas Evensen on 15/01/2025.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct WidgetEstimateBundle: WidgetBundle {
+    var body: some Widget {
+        WidgetEstimate()
+    }
+}
