77268d048cb7943069678f82759d213b31c1c76e
Thomas Evensen <thomeven@gmail.com>
2025-05-06 07:43:22 +0200

    update

--- RsyncUI/Model/Global/GlobalTimer.swift
+++ RsyncUI/Model/Global/GlobalTimer.swift
@@ -28,7 +28,7 @@ final class GlobalTimer {
         start()
     }
 
-    /*
+    
      func removeSchedule(name: String) {
          schedules.removeValue(forKey: name)
          if schedules.isEmpty {
@@ -36,7 +36,7 @@ final class GlobalTimer {
              timer = nil
          }
      }
-     */
+     
     func clearSchedules() {
         guard schedules.count > 0 else {
             Logger.process.info("GlobalTimer: clearSchedules() NO timer to invalidate")
