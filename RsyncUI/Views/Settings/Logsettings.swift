9917307a50f8cb78c3f0886bc5a62b3fac9a0f0b
Thomas Evensen <thomeven@gmail.com>
2025-05-12 10:07:57 +0200

    Update Logsettings.swift

--- RsyncUI/Views/Settings/Logsettings.swift
+++ RsyncUI/Views/Settings/Logsettings.swift
@@ -14,6 +14,8 @@ struct Logsettings: View {
     @State private var toggleobservemountedvolumes: Bool = false
     @State private var togglealwaysshowestimateddetailsview: Bool = false
     @State private var togglehideverifyremotefunction: Bool = false
+    
+    @State private var dataischanged: Bool = false
 
     var body: some View {
         Form {
@@ -21,44 +23,53 @@ struct Logsettings: View {
                 ToggleViewDefault(text: NSLocalizedString("Monitor network", comment: ""), binding: $logsettings.monitornetworkconnection)
                     .onChange(of: logsettings.monitornetworkconnection) {
                         SharedReference.shared.monitornetworkconnection = logsettings.monitornetworkconnection
+                        dataischanged = true
                     }
                 ToggleViewDefault(text: NSLocalizedString("Check for error in output", comment: ""), binding: $logsettings.checkforerrorinrsyncoutput)
                     .onChange(of: logsettings.checkforerrorinrsyncoutput) {
                         SharedReference.shared.checkforerrorinrsyncoutput = logsettings.checkforerrorinrsyncoutput
+                        dataischanged = true
                     }
                 ToggleViewDefault(text: NSLocalizedString("Add summary logrecord", comment: ""), binding: $logsettings.addsummarylogrecord)
                     .onChange(of: logsettings.addsummarylogrecord) {
                         SharedReference.shared.addsummarylogrecord = logsettings.addsummarylogrecord
+                        dataischanged = true
                     }
                 ToggleViewDefault(text: NSLocalizedString("No time delay Synchronize URL-actions", comment: ""), binding: $logsettings.synchronizewithouttimedelay)
                     .onChange(of: logsettings.synchronizewithouttimedelay) {
                         SharedReference.shared.synchronizewithouttimedelay = logsettings.synchronizewithouttimedelay
+                        dataischanged = true
                     }
                 ToggleViewDefault(text: NSLocalizedString("Hide the Sidebar on startup", comment: ""), binding: $logsettings.sidebarishidden)
                     .onChange(of: logsettings.sidebarishidden) {
                         SharedReference.shared.sidebarishidden = logsettings.sidebarishidden
+                        dataischanged = true
                     }
                 ToggleViewDefault(text: NSLocalizedString("Observe mounting of external drives", comment: ""), binding: $logsettings.observemountedvolumes)
                     .onChange(of: logsettings.observemountedvolumes) {
                         SharedReference.shared.observemountedvolumes = logsettings.observemountedvolumes
                         toggleobservemountedvolumes = logsettings.observemountedvolumes
+                        dataischanged = true
                     }
                 ToggleViewDefault(text: NSLocalizedString("Always present the summarized estimate view", comment: ""), binding: $logsettings.alwaysshowestimateddetailsview)
                     .onChange(of: logsettings.alwaysshowestimateddetailsview) {
                         SharedReference.shared.alwaysshowestimateddetailsview = logsettings.alwaysshowestimateddetailsview
                         togglealwaysshowestimateddetailsview = logsettings.alwaysshowestimateddetailsview
+                        dataischanged = true
                     }
 
                 ToggleViewDefault(text: NSLocalizedString("Hide Verify remote", comment: ""), binding: $logsettings.hideverifyremotefunction)
                     .onChange(of: logsettings.hideverifyremotefunction) {
                         SharedReference.shared.hideverifyremotefunction = logsettings.hideverifyremotefunction
                         togglehideverifyremotefunction = logsettings.hideverifyremotefunction
+                        dataischanged = true
                     }
 
                 if SharedReference.shared.rsyncversion3 {
                     ToggleViewDefault(text: NSLocalizedString("Confirm execute", comment: ""), binding: $logsettings.confirmexecute)
                         .onChange(of: logsettings.confirmexecute) {
                             SharedReference.shared.confirmexecute = logsettings.confirmexecute
+                            dataischanged = true
                         }
                 }
 
@@ -70,17 +81,20 @@ struct Logsettings: View {
                 Text("Monitor network, error and log settings")
             }
 
-            Section {
-                Button {
-                    _ = WriteUserConfigurationJSON(UserConfiguration())
-                    Logger.process.info("USER CONFIGURATION is SAVED")
-                } label: {
-                    Image(systemName: "square.and.arrow.down")
+            if dataischanged {
+                Section {
+                    Button {
+                        _ = WriteUserConfigurationJSON(UserConfiguration())
+                        Logger.process.info("USER CONFIGURATION is SAVED")
+                        dataischanged = false
+                    } label: {
+                        Image(systemName: "square.and.arrow.down")
+                    }
+                    .help("Save")
+                    .buttonStyle(ColorfulButtonStyle())
+                } header: {
+                    Text("Save userconfiguration")
                 }
-                .help("Save")
-                .buttonStyle(ColorfulButtonStyle())
-            } header: {
-                Text("Save userconfiguration")
             }
         }
         .formStyle(.grouped)
