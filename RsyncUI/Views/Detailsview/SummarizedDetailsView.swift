93f2ce70969adeac7b7700f1985468dace714979
Thomas Evensen <thomeven@gmail.com>
2025-06-02 19:11:27 +0200

    Update SummarizedDetailsView.swift

--- RsyncUI/Views/Detailsview/SummarizedDetailsView.swift
+++ RsyncUI/Views/Detailsview/SummarizedDetailsView.swift
@@ -113,7 +113,6 @@ struct SummarizedDetailsView: View {
                     Logger.process.warning("SummarizedDetailsView: estimate already in progress")
                     return
                 }
-                selecteduuids.removeAll()
                 estimateprogressdetails.resetcounts()
                 executeprogressdetails.estimatedlist = nil
                 estimateprogressdetails.startestimation()
