8bd629f5b662df4e812c422bb3a67883d1011184
Thomas Evensen <thomeven@gmail.com>
2025-06-07 16:31:38 +0200

    Update StateMachine.swift

--- RsyncUI/Views/Tasks/StateMachine.swift
+++ RsyncUI/Views/Tasks/StateMachine.swift
@@ -52,35 +52,37 @@ trafficLight.handle(event: .timer) // YELLOW -> RED
 import Foundation
 
 enum State {
-    case idle
-    case loading
-    case success
-    case failure
+    case noselecteduuid
+    case oneselecteduuid
+    case changedselecteduuid
+    case severalselecteduuid
+    case tasksisestimated
 }
 
 enum Event {
     case start
-    case succeed
-    case fail
-    case reset
+    case dryrun
+    case execute
+    case resetestimates
 }
 
 class StateMachine {
-    private(set) var state: State = .idle
+    
+    private(set) var state: State = .noselecteduuid
 
     func handle(event: Event) {
         switch (state, event) {
-        case (.idle, .start):
-            state = .loading
+        case (.noselecteduuid, .start):
+            state = .oneselecteduuid
             print("Transition: idle -> loading")
-        case (.loading, .succeed):
-            state = .success
+        case (.oneselecteduuid, .dryrun):
+            state = .severalselecteduuid
             print("Transition: loading -> success")
-        case (.loading, .fail):
-            state = .failure
+        case (.oneselecteduuid, .execute):
+            state = .changedselecteduuid
             print("Transition: loading -> failure")
-        case (.success, .reset), (.failure, .reset):
-            state = .idle
+        case (.severalselecteduuid, .resetestimates), (.changedselecteduuid, .resetestimates):
+            state = .noselecteduuid
             print("Transition: \(state) -> idle")
         default:
             print("No transition for (\(state), \(event))")
@@ -97,3 +99,62 @@ machine.handle(event: .start)    // idle -> loading
 machine.handle(event: .fail)     // loading -> failure
 machine.handle(event: .reset)    // failure -> idle
 */
+
+/*
+  
+  func dryrun() {
+      if selectedconfig.config != nil,
+         progressdetails.estimatedlist?.count ?? 0 == 0,
+         selectedtaskishalted == false
+      {
+          Logger.process.info("DryRun: execute a dryrun for one task only")
+          doubleclick = false
+          path.append(Tasks(task: .onetaskdetailsview))
+      } else if selectedconfig.config != nil,
+                progressdetails.executeanotherdryrun(rsyncUIdata.profile) == true
+      {
+          Logger.process.info("DryRun: new task same profile selected, execute a dryrun")
+          doubleclick = false
+          path.append(Tasks(task: .onetaskdetailsview))
+
+      } else if selectedconfig.config != nil,
+                progressdetails.alltasksestimated(rsyncUIdata.profile) == false
+      {
+          Logger.process.info("DryRun: profile is changed, new task selected, execute a dryrun")
+          doubleclick = false
+          path.append(Tasks(task: .onetaskdetailsview))
+      }
+  }
+
+  func execute() {
+      // All tasks are estimated and ready for execution.
+      rsyncUIdata.executetasksinprogress = true
+      if selecteduuids.count == 0,
+         progressdetails.alltasksestimated(rsyncUIdata.profile) == true
+      {
+          Logger.process.info("Execute() all estimated tasks")
+          // Execute all estimated tasks
+          selecteduuids = progressdetails.getuuidswithdatatosynchronize()
+          estimatestate.updateestimatestate(state: .start)
+          // Change view, see SidebarTasksView
+          path.append(Tasks(task: .executestimatedview))
+
+      } else if selecteduuids.count >= 1,
+                progressdetails.tasksareestimated(selecteduuids) == true
+
+      {
+          // One or some tasks are selected and estimated
+          Logger.process.info("Execute() estimated tasks only")
+          // Execute estimated tasks only
+          selecteduuids = progressdetails.getuuidswithdatatosynchronize()
+          estimatestate.updateestimatestate(state: .start)
+          // Change view, see SidebarTasksView
+          path.append(Tasks(task: .executestimatedview))
+      } else {
+          // Execute all tasks, no estimate
+          Logger.process.info("Execute() selected or all tasks NO estimate")
+          // Execute tasks, no estimate
+          showingAlert = true
+      }
+  }
+ */
