170b2eefaa38e51127646a9bb2932dc9734b826d
Thomas Evensen <thomeven@gmail.com>
2025-07-06 06:28:43 +0200

    Update MessageView to support light and dark mode
    MessageView now adapts its text color based on the current color scheme: green for dark mode and blue for light mode. This improves readability and visual consistency across themes.

--- RsyncUI/Views/Modifiers/Viewmodifiers.swift
+++ RsyncUI/Views/Modifiers/Viewmodifiers.swift
@@ -85,24 +85,44 @@ struct DismissafterMessageView: View {
 }
 
 struct MessageView: View {
+    @Environment(\.colorScheme) var colorScheme
+    
     private var mytext: String
     private var textsize: Font
 
     var body: some View {
-        ZStack {
-            RoundedRectangle(cornerRadius: 15).fill(Color.gray.opacity(0.3))
-            Text(mytext)
-                // .font(.caption2)
-                .font(textsize)
-                .foregroundColor(Color.green)
-                .multilineTextAlignment(.center)
-                .lineLimit(2)
-                .allowsTightening(false)
-                .minimumScaleFactor(0.5)
+        
+        if colorScheme == .dark {
+            ZStack {
+                RoundedRectangle(cornerRadius: 15).fill(Color.gray.opacity(0.3))
+                Text(mytext)
+                    // .font(.caption2)
+                    .font(textsize)
+                    .foregroundColor(Color.green)
+                    .multilineTextAlignment(.center)
+                    .lineLimit(2)
+                    .allowsTightening(false)
+                    .minimumScaleFactor(0.5)
+            }
+            .frame(height: 30, alignment: .center)
+            .background(RoundedRectangle(cornerRadius: 25).stroke(Color.gray, lineWidth: 1))
+            .padding()
+        } else {
+            ZStack {
+                RoundedRectangle(cornerRadius: 15).fill(Color.gray.opacity(0.3))
+                Text(mytext)
+                    // .font(.caption2)
+                    .font(textsize)
+                    .foregroundColor(Color.blue)
+                    .multilineTextAlignment(.center)
+                    .lineLimit(2)
+                    .allowsTightening(false)
+                    .minimumScaleFactor(0.5)
+            }
+            .frame(height: 30, alignment: .center)
+            .background(RoundedRectangle(cornerRadius: 25).stroke(Color.gray, lineWidth: 1))
+            .padding()
         }
-        .frame(height: 30, alignment: .center)
-        .background(RoundedRectangle(cornerRadius: 25).stroke(Color.gray, lineWidth: 1))
-        .padding()
     }
 
     init(mytext: String, size: Font) {
