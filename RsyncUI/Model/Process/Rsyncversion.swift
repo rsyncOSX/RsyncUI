d2c1a30f51e66488e160d82fec51cc70851e68e7
Thomas Evensen <thomeven@gmail.com>
2025-11-05 13:53:51 +0100

    Update Rsyncversion.swift

--- RsyncUI/Model/Process/Rsyncversion.swift
+++ RsyncUI/Model/Process/Rsyncversion.swift
@@ -65,8 +65,10 @@ extension Rsyncversion {
     func processtermination(stringoutputfromrsync: [String]?, hiddenID _: Int?) {
         guard stringoutputfromrsync?.count ?? 0 > 0 else { return }
         if let rsyncversionshort = stringoutputfromrsync?[0] {
-            SharedReference.shared.rsyncversionshort =
-                rsyncversionshort.replacingOccurrences(of: "protocol", with: "\nprotocol")
+            
+            let s = rsyncversionshort.replacingOccurrences(of: "protocol", with: "\nprotocol")
+            let result = s.replacingOccurrences(of: "(?s)Web site.*", with: "", options: .regularExpression)
+            SharedReference.shared.rsyncversionshort = result
 
             if rsyncversionshort.contains("version 3.") {
                 SharedReference.shared.rsyncversion3 = true
