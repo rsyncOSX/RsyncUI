9d410e95a44ad8088d2652107591c7cf1a2ee73b
Thomas Evensen <thomeven@gmail.com>
2025-11-06 11:01:17 +0100

    Update ActorLogChartsData.swift

--- RsyncUI/Model/Storage/Actors/ActorLogChartsData.swift
+++ RsyncUI/Model/Storage/Actors/ActorLogChartsData.swift
@@ -28,7 +28,7 @@ actor ActorLogChartsData {
         Logger.process.info("ActorLogChartsData: parselogrecords() MAIN THREAD: \(Thread.isMain, privacy: .public) but on \(Thread.current, privacy: .public)")
         Logger.process.info("ActorLogChartsData: number of records \(logrecords.count, privacy: .public)")
         // return logrecords.compactMap { logrecord in
-        return logrecords.map { logrecord in
+        return logrecords.compactMap { logrecord in
             let numbers = extractnumbersasdoubles(from: logrecord.resultExecuted ?? "")
 
             // Snapshot task
@@ -41,7 +41,7 @@ actor ActorLogChartsData {
                                 transferredMB: size,
                                 seconds: seconds)
                 // Synchronize task
-            } else {
+            } else if numbers.count ==  3 {
                 let files = numbers[0]
                 let size = numbers[1]
                 let seconds = numbers[2]
@@ -49,6 +49,8 @@ actor ActorLogChartsData {
                                 files: Int(files),
                                 transferredMB: size,
                                 seconds: seconds)
+            } else {
+                return nil
             }
         }
     }
