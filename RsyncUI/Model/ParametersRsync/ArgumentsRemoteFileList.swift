e5153e7d5ac810830493fa788ffb31eef29d4362
Thomas Evensen <thomeven@gmail.com>
2025-11-20 20:56:32 +0100

    Refactor parameter construction in rsync argument classes
    Replaced manual Parameters initialization with Params().params(config:) in all rsync argument classes for consistency and maintainability. Removed the unused PrepareParameters.swift file.

--- RsyncUI/Model/ParametersRsync/ArgumentsRemoteFileList.swift
+++ RsyncUI/Model/ParametersRsync/ArgumentsRemoteFileList.swift
@@ -15,39 +15,7 @@ final class ArgumentsRemoteFileList {
 
     func remotefilelistarguments() -> [String]? {
         if let config {
-            let params = Parameters(
-                task: config.task,
-                basicParameters: BasicRsyncParameters(
-                    archiveMode: "--archive",
-                    verboseOutput: "--verbose",
-                    compressionEnabled: "--compress",
-                    deleteExtraneous: "--delete"
-                ),
-                optionalParameters: OptionalRsyncParameters(parameter8: config.parameter8,
-                                                            parameter9: config.parameter9,
-                                                            parameter10: config.parameter10,
-                                                            parameter11: config.parameter11,
-                                                            parameter12: config.parameter12,
-                                                            parameter13: config.parameter13,
-                                                            parameter14: config.parameter14),
-
-                sshParameters: SSHParameters(
-                    offsiteServer: config.offsiteServer,
-                    offsiteUsername: config.offsiteUsername,
-                    sshport: String(config.sshport ?? -1),
-                    sshkeypathandidentityfile: config.sshkeypathandidentityfile ?? "",
-                    sharedsshport: String(SharedReference.shared.sshport ?? -1),
-                    sharedsshkeypathandidentityfile: SharedReference.shared.sshkeypathandidentityfile,
-                    rsyncversion3: SharedReference.shared.rsyncversion3
-                ),
-                paths: PathConfiguration(
-                    localCatalog: config.localCatalog,
-                    offsiteCatalog: config.offsiteCatalog
-                ),
-                snapshotNumber: config.snapshotnum,
-                isRsyncDaemon: false, // Use Bool instead of -1/1
-                rsyncVersion3: SharedReference.shared.rsyncversion3
-            )
+            let params = Params().params(config: config)
             let rsyncparametersremotelist = RsyncParametersRestore(parameters: params)
             if config.task == SharedReference.shared.synchronize {
                 do {
