4394c4ebcdaedd25a6154dbe8bbf79f1252ba81a
timreichen <tim.reichen@gmail.com>
2026-07-06 13:39:59 +0200

    initial commit

--- RsyncUI/Views/Configurations/ListofTasksAddView.swift
+++ RsyncUI/Views/Configurations/ListofTasksAddView.swift
@@ -21,7 +21,7 @@ struct ListofTasksAddView: View {
             .confirmationDialog(selecteduuids.count == 1 ? "Delete 1 configuration" :
                 "Delete \(selecteduuids.count) configurations",
                 isPresented: $confirmdelete) {
-                    Button("Delete") {
+                Button("Delete", role: .destructive) {
                         delete()
                         confirmdelete = false
                     }
